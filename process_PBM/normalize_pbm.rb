26378936de044273447b1761278692bd29c7a4a8
Ilya Vorontsov <vorontsov.i.e@gmail.com>
2020-05-18 19:40:47 +0300

    option to add linker to a sequence

--- process_PBM/normalize_pbm.rb
+++ process_PBM/normalize_pbm.rb
@@ -7,9 +7,11 @@ require_relative 'quantile_normalization'
 require 'optparse'
 
 normalization_mode = :log10_scaled
+linker_length = 0 # Take `linker_length` nucleotides from linker sequence
 option_parser = OptionParser.new{|opts|
   opts.on('--log10') { normalization_mode = :log10_scaled }
   opts.on('--log10-bg') { normalization_mode = :log10_scaled_bg_normalized }
+  opts.on('--linker-length LENGTH') {|val| linker_length = Integer(val) }
 }
 
 FileUtils.mkdir_p('results/seq_zscore')
@@ -32,8 +34,8 @@ chips_by_type.each{|chip_type, fns|
   zscored_chips.each{|chip|
     File.open("results/seq_zscore/#{chip.basename}.tsv", 'w') {|fw|
       chip.probes.sort_by(&:signal).reverse.each{|probe|
-        # info = [probe.id_probe, probe.linker_sequence[-3..-1] + probe.pbm_sequence, probe.signal]
-        info = [probe.id_probe, probe.pbm_sequence, probe.signal]
+        linker_suffix = (linker_length == 0) ? '' : probe.linker_sequence[(-linker_length) .. (-1)]
+        info = [probe.id_probe, linker_suffix + probe.pbm_sequence, probe.signal]
         fw.puts(info.join("\t"))
       }
     }
