bf02eedd1c11ebf7284c760de8defcc10ca9d59f
Ilya Vorontsov <vorontsov.i.e@gmail.com>
2020-05-18 17:41:10 +0300

    added linker sequence at correlation estimation stage

--- process_PBM/process.sh
+++ process_PBM/process.sh
@@ -78,8 +78,8 @@ echo -e "chip\tcorrelation" > results/motif_qualities.tsv
 echo -e "chip\tcorrelation" > results/motif_qualities_zscored.tsv
 for FN in $( find results/top_seqs/ -xtype f ); do
   BN=$(basename -s .fa ${FN})
-  cat data/RawData/${BN}.txt | awk -F $'\t' -e '{print $8 "\t" $6}' | tail -n+2 > results/chip_scores_for_benchmark/${BN}.txt
-  cat results/zscored_chips/${BN}.txt | awk -F $'\t' -e '{print $8 "\t" $6}' | tail -n+2 > results/chip_scores_for_benchmark_zscored/${BN}.txt
+  cat data/RawData/${BN}.txt | awk -F $'\t' -e '{print $8 "\t" $7$6}' | tail -n+2 > results/chip_scores_for_benchmark/${BN}.txt
+  cat results/zscored_chips/${BN}.txt | awk -F $'\t' -e '{print $8 "\t" $7$6}' | tail -n+2 > results/chip_scores_for_benchmark_zscored/${BN}.txt
   CORRELATION=$(docker run --rm \
       --security-opt apparmor=unconfined \
       --mount type=bind,src=$(pwd)/results/chip_scores_for_benchmark/${BN}.txt,dst=/pbm_data.txt,readonly \
