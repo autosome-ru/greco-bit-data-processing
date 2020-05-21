5d62ac288db86918e811aa6871b19b637ae07a74
Ilya Vorontsov <vorontsov.i.e@gmail.com>
2020-05-21 15:01:23 +0300

    remove flagged sequences

--- process_PBM/chip_sequences.rb
+++ process_PBM/chip_sequences.rb
@@ -24,7 +24,7 @@ Dir.glob(File.join(src_folder, '*.txt')).each{|fn|
   chip = Chip.from_file(fn)
 
   File.open(File.join(dst_folder, "#{chip.basename}.tsv"), 'w') {|fw|
-    chip.probes.sort_by(&:signal).reverse.each{|probe|
+    chip.probes.reject(&:flag).sort_by(&:signal).reverse.each{|probe|
       linker_suffix = (linker_length == 0) ? '' : probe.linker_sequence[(-linker_length) .. (-1)]
       info = [probe.id_probe, linker_suffix + probe.pbm_sequence, probe.signal]
       fw.puts(info.join("\t"))
