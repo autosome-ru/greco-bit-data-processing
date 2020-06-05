18adec72c8293b82052a08ff41790dad2974a0ec
Ilya Vorontsov <vorontsov.i.e@gmail.com>
2020-06-05 12:56:53 +0300

    added affiseq metrics; source/results folder are now affiseq-specific

--- process_peaks_CHS_AFS/process.sh
+++ process_peaks_CHS_AFS/process.sh
@@ -1,16 +1,17 @@
-# ln -s /home_local/ivanyev/egrid/dfs-affyseq-cutadapt/peaks-interval source_data/peaks-intervals
+#!/usr/bin/env bash
+SOURCE_FOLDER="./source_data/affiseq"
+RESULTS_FOLDER="./results/affiseq/"
+# ln -s /home_local/ivanyev/egrid/dfs-affyseq-cutadapt/peaks-interval "${SOURCE_FOLDER}/peaks-intervals"
 
-mkdir -p results
-cat source_data/metrics_by_exp.tsv | tail -n+2 | cut -d $'\t' -f2,4 | awk -F $'\t' -e '$1 != "CONTROL" && $1 != ""' > results/tf_peaks.txt
+mkdir -p "${RESULTS_FOLDER}"
+cat "${SOURCE_FOLDER}/metrics_by_exp.tsv" | tail -n+2 | cut -d $'\t' -f2,4 | awk -F $'\t' -e '$1 != "CONTROL" && $1 != ""' > "${RESULTS_FOLDER}/tf_peaks.txt"
 
 TRAIN_CHR="$(seq 1 2 21)" # odd
-VALIDATION_CHR="$(seq 2 2 22)" #even
+VALIDATION_CHR="$(seq 2 2 22)" # even
 
 for PEAK_CALLER in cpics gem macs2-pemode sissrs; do
-	for FN in $(find source_data/peaks-intervals/${PEAK_CALLER} -xtype f -iname '*.interval'); do
-		BN=$( basename -s .interval)
-		ruby split_train_val.rb "${FN}" "./results/train/peaks-intervals/${PEAK_CALLER}/${BN}.interval" "./results/validation/peaks-intervals/${PEAK_CALLER}/${BN}.interval"
-
+	for FN in $(find "${SOURCE_FOLDER}/peaks-intervals/${PEAK_CALLER}" -xtype f -iname '*.interval'); do
+		BN="$( basename -s .interval )"
+		ruby split_train_val.rb "${FN}" "${RESULTS_FOLDER}/train/peaks-intervals/${PEAK_CALLER}/${BN}.interval" "${RESULTS_FOLDER}/validation/peaks-intervals/${PEAK_CALLER}/${BN}.interval"
 	done
 done
-
