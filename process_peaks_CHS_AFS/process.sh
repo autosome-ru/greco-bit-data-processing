f98a9838876dcbd174293038e21a417fb8f1a2f0
Ilya Vorontsov <vorontsov.i.e@gmail.com>
2020-06-05 12:59:06 +0300

    fix

--- process_peaks_CHS_AFS/process.sh
+++ process_peaks_CHS_AFS/process.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+set -euo pipefail
+
 SOURCE_FOLDER="./source_data/affiseq"
 RESULTS_FOLDER="./results/affiseq/"
 # ln -s /home_local/ivanyev/egrid/dfs-affyseq-cutadapt/peaks-interval "${SOURCE_FOLDER}/peaks-intervals"
@@ -11,7 +13,7 @@ VALIDATION_CHR="$(seq 2 2 22)" # even
 
 for PEAK_CALLER in cpics gem macs2-pemode sissrs; do
 	for FN in $(find "${SOURCE_FOLDER}/peaks-intervals/${PEAK_CALLER}" -xtype f -iname '*.interval'); do
-		BN="$( basename -s .interval )"
+		BN="$( basename -s .interval "${FN}")"
 		ruby split_train_val.rb "${FN}" "${RESULTS_FOLDER}/train/peaks-intervals/${PEAK_CALLER}/${BN}.interval" "${RESULTS_FOLDER}/validation/peaks-intervals/${PEAK_CALLER}/${BN}.interval"
 	done
 done
