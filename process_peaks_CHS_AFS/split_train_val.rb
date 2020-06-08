f0e144e6c35788aa149c3848ab100cf89b0b0ae4
Ilya Vorontsov <vorontsov.i.e@gmail.com>
2020-06-08 12:55:36 +0300

    simplify split train/val (don't parse tables as they have different formats)

--- process_peaks_CHS_AFS/split_train_val.rb
+++ process_peaks_CHS_AFS/split_train_val.rb
@@ -1,45 +1,22 @@
 require 'set'
 require 'fileutils'
 
-#CHROM  START END abs_summit  pileup  -log10(pvalue)  fold_enrichment -log10(qvalue)  name
-PeakInfo = Struct.new(:chr, :start, :stop, :abs_summit, :pileup, :neg_log10_pval, :fold_enrichment, :neg_log10_qval, :name) do
-  HEADER = ['CHROM', 'START', 'END', 'abs_summit', 'pileup', '-log10(pvalue)', 'fold_enrichment', '-log10(qvalue)', 'name',]
-  def self.from_string(str)
-    chr, start, stop, abs_summit, pileup, neg_log10_pval, fold_enrichment, neg_log10_qval, name = str.chomp.split("\t")
-    self.new(chr, Integer(start), Integer(stop), Integer(abs_summit), Integer(pileup), Float(neg_log10_pval), Float(fold_enrichment), Float(neg_log10_qval), name)
-  end
+def read_table(filename)
+  header, rows = nil, nil
+  File.open(filename) {|f|
+    header = f.readline.chomp.split("\t")
+    rows = f.readlines.map{|l| l.chomp.split("\t") }
+  }
+  [header, rows]
+end
 
-  def self.each_in_stream(stream, has_header: true, &block)
-    return enum_for(:each_in_stream, stream, has_header: has_header)  unless block_given?
-    stream.readline  if has_header # skip header
-    stream.each_line{|l|
-      yield self.from_string(l)
+def store_table(filename, header, rows)
+  File.open(filename, 'w'){|fw|
+    fw.puts(header.join("\t"))
+    rows.each{|row|
+      fw.puts(row.join("\t"))
     }
-  end
-
-  def self.each_in_file(filename, has_header: true, &block)
-    return enum_for(:each_in_file, filename, has_header: has_header)  unless block_given?
-    File.open(filename){|f|
-      self.each_in_stream(f, has_header: has_header, &block)
-    }
-  end
-
-  def to_s
-    [chr, start, stop, abs_summit, pileup, neg_log10_pval, fold_enrichment, neg_log10_qval, name].join("\t")
-  end
-
-  def self.output_to_stream(peaks, stream, has_header: true)
-    stream.puts('#' + HEADER.join("\t")) if has_header
-    peaks.each{|peak|
-      stream.puts(peak)
-    }
-  end
-
-  def self.store(peaks, filename, has_header: true)
-    File.open(filename){|fw|
-      self.output_to_stream(peaks, fw, has_header: has_header)
-    }
-  end
+  }
 end
 
 TRAIN_CHR = (1..21).step(2).to_set
@@ -49,9 +26,9 @@ peaks_fn, train_fn, validation_fn = ARGV.first(3)
 FileUtils.mkdir_p(File.dirname(train_fn))
 FileUtils.mkdir_p(File.dirname(validation_fn))
 
-peaks = PeakInfo.each_in_file(peaks_fn)
+header, peaks = read_table(peaks_fn)
 
-train_peaks = peaks.select{|peak| TRAIN_CHR.include?(peak.chr) }
-validation_peaks = peaks.select{|peak| VALIDATION_CHR.include?(peak.chr) }
-PeakInfo.store(train_peaks, train_fn, has_header: true)
-PeakInfo.store(validation_peaks, validation_fn, has_header: true)
+train_peaks = peaks.select{|peak_row| chr = peak_row[0]; TRAIN_CHR.include?(chr) }
+validation_peaks = peaks.select{|peak_row| chr = peak_row[0]; VALIDATION_CHR.include?(chr) }
+store_table(train_fn, header, train_peaks)
+store_table(validation_fn, header, validation_peaks)
