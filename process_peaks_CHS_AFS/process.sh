544f3d8e8616806bc0c5a21e663d643fe91445a8
Ilya Vorontsov <vorontsov.i.e@gmail.com>
2020-06-11 14:18:07 +0000

    remove train/test chromosomes from bash-script

--- process_peaks_CHS_AFS/process.sh
+++ process_peaks_CHS_AFS/process.sh
@@ -8,9 +8,6 @@ RESULTS_FOLDER="./results/affiseq/"
 mkdir -p "${RESULTS_FOLDER}"
 cat "${SOURCE_FOLDER}/metrics_by_exp.tsv" | tail -n+2 | cut -d $'\t' -f2,4 | awk -F $'\t' -e '$1 != "CONTROL" && $1 != ""' > "${RESULTS_FOLDER}/tf_peaks.txt"
 
-TRAIN_CHR="$(seq 1 2 21)" # odd
-VALIDATION_CHR="$(seq 2 2 22)" # even
-
 for PEAK_CALLER in cpics gem macs2-pemode sissrs; do
 	for FN in $(find "${SOURCE_FOLDER}/peaks-intervals/${PEAK_CALLER}" -xtype f -iname '*.interval'); do
 		BN="$( basename -s .interval "${FN}")"
