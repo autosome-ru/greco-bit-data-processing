fd8f98b4e70bd2cb57a525d1677f94e353d6e1d9
Ilya Vorontsov <vorontsov.i.e@gmail.com>
2020-06-12 07:34:17 +0000

    install bedtools to a local folder

--- process_peaks_CHS_AFS/process.sh
+++ process_peaks_CHS_AFS/process.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# wget https://github.com/arq5x/bedtools2/releases/download/v2.29.2/bedtools-2.29.2.tar.gz
+# tar -zxf bedtools-2.29.2.tar.gz && rm bedtools-2.29.2.tar.gz
+# cd bedtools2/ && make && cd .. && ln -s bedtools2/bin/bedtools bedtools
+
 SOURCE_FOLDER="./source_data/affiseq"
 RESULTS_FOLDER="./results/affiseq/"
 # ln -s /home_local/ivanyev/egrid/dfs-affyseq-cutadapt/peaks-interval "${SOURCE_FOLDER}/peaks-intervals"
