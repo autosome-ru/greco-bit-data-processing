19c1192b2efe64b83ff37ad72aa65e0895fb02de
Ilya Vorontsov <vorontsov.i.e@gmail.com>
2020-07-26 19:22:59 +0300

    _chips --> _intenstities

--- process_PBM/process_data.sh
+++ process_PBM/process_data.sh
@@ -9,84 +9,84 @@ NORMALIZATION_OPTS='--log10'
 INTERMEDIATE_FOLDER='results_databox_intermediate'
 RESULTS_FOLDER='results_databox'
 
-mkdir -p ${INTERMEDIATE_FOLDER}/raw_chips/
+mkdir -p ${INTERMEDIATE_FOLDER}/raw_intensities/
 for FN in $(find -L $CHIPS_SOURCE_FOLDER -xtype f -iname '*.txt'); do
     BN=$(basename -s .txt ${FN})
     TF=$(echo ${BN} | grep -oPe '[^_]+$' | grep -oPe '^[^.]+')
-    cp ${FN} ${INTERMEDIATE_FOLDER}/raw_chips/${TF}.${BN}.pbm.txt
+    cp ${FN} ${INTERMEDIATE_FOLDER}/raw_intensities/${TF}.${BN}.pbm.txt
 done
 
-## sd_qn_chips
+## sd_qn_intensities
 # window-size=5 means window 11x11
-./spatial_detrending.sh --source ${INTERMEDIATE_FOLDER}/raw_chips/ \
-                        --destination ${INTERMEDIATE_FOLDER}/spatial_detrended_chips/ \
+./spatial_detrending.sh --source ${INTERMEDIATE_FOLDER}/raw_intensities/ \
+                        --destination ${INTERMEDIATE_FOLDER}/spatial_detrended_intensities/ \
                         --window-size 5 \
                         --num-threads ${NUM_THREADS}
 
 ruby quantile_normalize_chips.rb \
         ${NORMALIZATION_OPTS} \
-        --source ${INTERMEDIATE_FOLDER}/spatial_detrended_chips/ \
-        --destination ${INTERMEDIATE_FOLDER}/sd_qn_chips
+        --source ${INTERMEDIATE_FOLDER}/spatial_detrended_intensities/ \
+        --destination ${INTERMEDIATE_FOLDER}/sd_qn_intensities
 
-## qn_zscore_chips
+## qn_zscore_intensities
 ruby quantile_normalize_chips.rb \
         ${NORMALIZATION_OPTS} \
-        --source ${INTERMEDIATE_FOLDER}/raw_chips/ \
-        --destination ${INTERMEDIATE_FOLDER}/quantile_normalized_chips
+        --source ${INTERMEDIATE_FOLDER}/raw_intensities/ \
+        --destination ${INTERMEDIATE_FOLDER}/quantile_normalized_intensities
 
 ruby zscore_transform_chips.rb \
-        --source ${INTERMEDIATE_FOLDER}/quantile_normalized_chips \
-        --destination ${INTERMEDIATE_FOLDER}/qn_zscore_chips
+        --source ${INTERMEDIATE_FOLDER}/quantile_normalized_intensities \
+        --destination ${INTERMEDIATE_FOLDER}/qn_zscore_intensities
 
 #########
 
-mkdir -p ${RESULTS_FOLDER}/raw/train_chips  ${RESULTS_FOLDER}/raw/validation_chips
-mkdir -p ${RESULTS_FOLDER}/spatialDetrend_quantNorm/train_chips  ${RESULTS_FOLDER}/spatialDetrend_quantNorm/validation_chips 
-mkdir -p ${RESULTS_FOLDER}/quantNorm_zscore/train_chips  ${RESULTS_FOLDER}/quantNorm_zscore/validation_chips
+mkdir -p ${RESULTS_FOLDER}/raw/train_intensities  ${RESULTS_FOLDER}/raw/validation_intensities
+mkdir -p ${RESULTS_FOLDER}/spatialDetrend_quantNorm/train_intensities  ${RESULTS_FOLDER}/spatialDetrend_quantNorm/validation_intensities 
+mkdir -p ${RESULTS_FOLDER}/quantNorm_zscore/train_intensities  ${RESULTS_FOLDER}/quantNorm_zscore/validation_intensities
 
 # raw
-for FN in $(find ${INTERMEDIATE_FOLDER}/raw_chips/ -xtype f -name '*_1M-ME_*'); do
+for FN in $(find ${INTERMEDIATE_FOLDER}/raw_intensities/ -xtype f -name '*_1M-ME_*'); do
     BN=$(basename -s .txt ${FN})
-    cp ${FN} ${RESULTS_FOLDER}/raw/train_chips/${BN}.train.txt
+    cp ${FN} ${RESULTS_FOLDER}/raw/train_intensities/${BN}.train.txt
 done
 
-for FN in $(find ${INTERMEDIATE_FOLDER}/raw_chips/ -xtype f -name '*_1M-HK_*'); do
+for FN in $(find ${INTERMEDIATE_FOLDER}/raw_intensities/ -xtype f -name '*_1M-HK_*'); do
     BN=$(basename -s .txt ${FN})
-    cp ${FN} ${RESULTS_FOLDER}/raw/validation_chips/${BN}.val.txt
+    cp ${FN} ${RESULTS_FOLDER}/raw/validation_intensities/${BN}.val.txt
 done
 
 # sd_qn
-for FN in $(find ${INTERMEDIATE_FOLDER}/sd_qn_chips/ -xtype f -name '*_1M-ME_*'); do
+for FN in $(find ${INTERMEDIATE_FOLDER}/sd_qn_intensities/ -xtype f -name '*_1M-ME_*'); do
     BN=$(basename -s .txt ${FN})
-    cp ${FN} ${RESULTS_FOLDER}/spatialDetrend_quantNorm/train_chips/${BN}.train.txt
+    cp ${FN} ${RESULTS_FOLDER}/spatialDetrend_quantNorm/train_intensities/${BN}.train.txt
 done
 
-for FN in $(find ${INTERMEDIATE_FOLDER}/sd_qn_chips/ -xtype f -name '*_1M-HK_*'); do
+for FN in $(find ${INTERMEDIATE_FOLDER}/sd_qn_intensities/ -xtype f -name '*_1M-HK_*'); do
     BN=$(basename -s .txt ${FN})
-    cp ${FN} ${RESULTS_FOLDER}/spatialDetrend_quantNorm/validation_chips/${BN}.val.txt
+    cp ${FN} ${RESULTS_FOLDER}/spatialDetrend_quantNorm/validation_intensities/${BN}.val.txt
 done
 
 # qn_zscore
-for FN in $(find ${INTERMEDIATE_FOLDER}/qn_zscore_chips/ -xtype f -name '*_1M-ME_*'); do
+for FN in $(find ${INTERMEDIATE_FOLDER}/qn_zscore_intensities/ -xtype f -name '*_1M-ME_*'); do
     BN=$(basename -s .txt ${FN})
-    cp ${FN} ${RESULTS_FOLDER}/quantNorm_zscore/train_chips/${BN}.train.txt
+    cp ${FN} ${RESULTS_FOLDER}/quantNorm_zscore/train_intensities/${BN}.train.txt
 done
 
-for FN in $(find ${INTERMEDIATE_FOLDER}/qn_zscore_chips/ -xtype f -name '*_1M-HK_*'); do
+for FN in $(find ${INTERMEDIATE_FOLDER}/qn_zscore_intensities/ -xtype f -name '*_1M-HK_*'); do
     BN=$(basename -s .txt ${FN})
-    cp ${FN} ${RESULTS_FOLDER}/quantNorm_zscore/validation_chips/${BN}.val.txt
+    cp ${FN} ${RESULTS_FOLDER}/quantNorm_zscore/validation_intensities/${BN}.val.txt
 done
 
 
 for SUBFOLDER in raw  spatialDetrend_quantNorm  quantNorm_zscore; do
     ruby chip_sequences.rb \
-            --source ${RESULTS_FOLDER}/${SUBFOLDER}/train_chips \
+            --source ${RESULTS_FOLDER}/${SUBFOLDER}/train_intensities \
             --destination ${RESULTS_FOLDER}/${SUBFOLDER}/train_sequences \
             --linker-length 0 \
             --fasta  --take-top 1000
 
     ruby chip_sequences.rb \
-            --source ${RESULTS_FOLDER}/${SUBFOLDER}/validation_chips \
+            --source ${RESULTS_FOLDER}/${SUBFOLDER}/validation_intensities \
             --destination ${RESULTS_FOLDER}/${SUBFOLDER}/validation_sequences \
             --linker-length 0 \
             --fasta  --take-top 1000
