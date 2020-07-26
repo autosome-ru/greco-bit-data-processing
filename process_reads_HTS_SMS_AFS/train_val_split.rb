245a745efd0a6d685ae5537191cadf209493c25d
Ilya Vorontsov <vorontsov.i.e@gmail.com>
2020-07-26 18:06:34 +0300

    IVT/Lysate go to separate folders

--- process_reads_HTS_SMS_AFS/train_val_split.rb
+++ process_reads_HTS_SMS_AFS/train_val_split.rb
@@ -38,34 +38,36 @@ def parse_filename(filename)
   {tf: tf, adapter: adapter, type: type, batch: batch, cycle: cycle, read: read, filename: filename, basename: basename}
 end
 
-results_folder = 'results'
-FileUtils.mkdir_p "#{results_folder}/train_reads"
-FileUtils.mkdir_p "#{results_folder}/validation_reads"
+['IVT', 'Lysate'].each{|experiment_type|
+  results_folder = "results_#{experiment_type}"
+  FileUtils.mkdir_p "#{results_folder}/train_reads"
+  FileUtils.mkdir_p "#{results_folder}/validation_reads"
 
-sample_filenames = Dir.glob('source_data/reads/*.fastq.gz')
-sample_filenames.select!{|fn| File.basename(fn).match?(/_(IVT|Lysate)_/) }
-sample_filenames.reject!{|fn| File.basename(fn).match?(/_AffSeq_/) }
+  sample_filenames = Dir.glob('source_data/reads/*.fastq.gz')
+  sample_filenames.select!{|fn| File.basename(fn).match?(/_#{experiment_type}_/) }
+  sample_filenames.reject!{|fn| File.basename(fn).match?(/_AffSeq_/) }
 
-samples = sample_filenames.map{|fn| parse_filename(fn) }
-Parallel.each(samples, in_processes: 20) do |sample|
-  # In SELEX there are no paired reads, so we don't add it to filename
-  bn = sample.values_at(:tf, :type, :cycle, :adapter, :batch).join('.')
-  train_fn = "#{results_folder}/train_reads/#{bn}.selex.train.fastq.gz"
-  validation_fn = "#{results_folder}/validation_reads/#{bn}.selex.val.fastq.gz"
-  train_val_split(sample[:filename], train_fn, validation_fn)
-end
-
-File.open('results/stats.tsv', 'w') do |fw|
-  header = ['tf', 'type', 'cycle', 'adapter', 'batch', 'train/validation', 'filename', 'num_reads']
-  fw.puts(header.join("\t"))
-  samples.each{|sample|
+  samples = sample_filenames.map{|fn| parse_filename(fn) }
+  Parallel.each(samples, in_processes: 20) do |sample|
+    # In SELEX there are no paired reads, so we don't add it to filename
     bn = sample.values_at(:tf, :type, :cycle, :adapter, :batch).join('.')
     train_fn = "#{results_folder}/train_reads/#{bn}.selex.train.fastq.gz"
-    info_train = sample.values_at(:tf, :type, :cycle, :adapter, :batch) + ['train', train_fn, num_reads_in_fastq(train_fn)]
-    fw.puts(info_train.join("\t"))
-
     validation_fn = "#{results_folder}/validation_reads/#{bn}.selex.val.fastq.gz"
-    info_validation = sample.values_at(:tf, :type, :cycle, :adapter, :batch) + ['validation', validation_fn, num_reads_in_fastq(validation_fn)]
-    fw.puts(info_validation.join("\t"))
-  }
-end
+    train_val_split(sample[:filename], train_fn, validation_fn)
+  end
+
+  File.open('results/stats.tsv', 'w') do |fw|
+    header = ['tf', 'type', 'cycle', 'adapter', 'batch', 'train/validation', 'filename', 'num_reads']
+    fw.puts(header.join("\t"))
+    samples.each{|sample|
+      bn = sample.values_at(:tf, :type, :cycle, :adapter, :batch).join('.')
+      train_fn = "#{results_folder}/train_reads/#{bn}.selex.train.fastq.gz"
+      info_train = sample.values_at(:tf, :type, :cycle, :adapter, :batch) + ['train', train_fn, num_reads_in_fastq(train_fn)]
+      fw.puts(info_train.join("\t"))
+
+      validation_fn = "#{results_folder}/validation_reads/#{bn}.selex.val.fastq.gz"
+      info_validation = sample.values_at(:tf, :type, :cycle, :adapter, :batch) + ['validation', validation_fn, num_reads_in_fastq(validation_fn)]
+      fw.puts(info_validation.join("\t"))
+    }
+  end
+}
