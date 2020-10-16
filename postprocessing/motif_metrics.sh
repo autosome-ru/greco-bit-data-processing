6c004c3c86fa2fdae7c5ebb574b3f9142c02740e
Ilya Vorontsov <vorontsov.i.e@gmail.com>
2020-10-16 23:17:14 +0000

    selex benchmark configurable top fraction

--- postprocessing/motif_metrics.sh
+++ postprocessing/motif_metrics.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 mkdir -p results
 ruby motif_metrics_pbm.rb | parallel > results/pbm_metrics.txt
-ruby motif_metrics_selex.rb | parallel > results/selex_metrics.txt
+ruby motif_metrics_selex.rb --fraction 0.1 | parallel > results/selex_0.1_metrics.txt
+ruby motif_metrics_selex.rb --fraction 0.5 | parallel > results/selex_0.5_metrics.txt
 ruby motif_metrics_chipseq_affiseq.rb | parallel > results/chipseq_affiseq_metrics.txt
+ruby motif_metrics_VIGG_peaks.rb | parallel > results/vigg_peaks_metrics.txt
 
 # ROCLOG and PRLOG are equivalently ranked to ROC/PR so we don't care about them
 cat results/pbm_metrics.txt | ruby -r json -e 'metrics = %w[ASIS LOG EXP ROC PR]; puts(["dataset", "motif", *metrics].join("\t")); $stdin.each_line{|l| ds, mot, info = l.chomp.split("\t"); puts([File.basename(ds), File.basename(mot), JSON.parse(info).values_at(*metrics)].join("\t")) }' > results/parsed_pbm_metrics.tsv
 cat results/selex_metrics.txt | ruby -e 'puts(["dataset", "motif", "AUCROC"].join("\t")); $stdin.each_line{|l| ds, mot, aucroc = l.chomp.split("\t"); puts([File.basename(ds), File.basename(mot), aucroc].join("\t")) }' > results/parsed_selex_metrics.tsv
 cat results/chipseq_affiseq_metrics.txt | ruby -e 'puts(["dataset", "motif", "AUCROC"].join("\t")); $stdin.each_line{|l| ds, mot, aucroc = l.chomp.split("\t"); puts([File.basename(ds), File.basename(mot), aucroc].join("\t")) }' > results/parsed_chipseq_affiseq_metrics.tsv
-
+cat results/vigg_peaks_metrics.txt | ruby -r json -e 'metrics = %w[roc_auc logroc_auc]; puts(["dataset", "motif", *metrics].join("\t")); $stdin.each_line{|l| ds, mot, info = l.chomp.split("\t"); puts([File.basename(ds), File.basename(mot), JSON.parse(info)["metrics"].values_at(*metrics)].join("\t")) }' > results/parsed_vigg_peaks_metrics.tsv
--- postprocessing/motif_metrics_selex.rb
+++ postprocessing/motif_metrics_selex.rb
@@ -1,6 +1,14 @@
 require 'fileutils'
+require 'optparse'
 
 Signal.trap("PIPE", "EXIT")
+
+top_fraction = 0.1
+option_parser = OptionParser.new{|opts|
+  opts.on('--fraction X', "Take top X sequences"){|val|
+    top_fraction = Float(val)
+  }
+}
 
 DATA_PATH = '/home_local/vorontsovie/greco-data/release_3.2020-08-08/'
 
@@ -56,7 +64,7 @@ tfs.each{|tf|
         " --volume #{dataset_fq}:/seq.fastq.gz:ro " \
         " --volume #{motif}:/motif#{ext}:ro " \
         " vorontsovie/pwmeval_selex:1.0.0 " \
-        " --seq-length 40 --non-redundant --top 0.1 --bin 1000 " \
+        " --seq-length 40 --non-redundant --top #{top_fraction} --bin 1000 " \
         " --pseudo-weight 0.0001 --flank-5 #{flank_5} --flank-3 #{flank_3} " \
         " --seed 1 "
       puts cmd_2
