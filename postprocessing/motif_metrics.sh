05e3200bc889bda0b4e95be9b0f268c3a3828795
Ilya Vorontsov <vorontsov.i.e@gmail.com>
2020-10-27 08:40:54 +0000

    extract MERS/LOGMERS for PBMs

--- postprocessing/motif_metrics.sh
+++ postprocessing/motif_metrics.sh
@@ -10,7 +10,7 @@ ruby motif_metrics_chipseq_affiseq.rb  ${DATA_FOLDER}  ${MOTIFS_FOLDER}  | paral
 ruby motif_metrics_VIGG_peaks.rb  ${DATA_FOLDER}  ${MOTIFS_FOLDER}  | parallel > results/vigg_peaks_metrics.txt
 
 # ROCLOG and PRLOG are equivalently ranked to ROC/PR so we don't care about them
-cat results/pbm_metrics.txt | ruby -r json -e 'metrics = %w[ASIS LOG EXP ROC PR]; puts(["dataset", "motif", *metrics].join("\t")); $stdin.each_line{|l| ds, mot, info = l.chomp.split("\t"); puts([File.basename(ds), File.basename(mot), JSON.parse(info).values_at(*metrics)].join("\t")) }' > results/parsed_pbm_metrics.tsv
+cat results/pbm_metrics.txt | ruby -r json -e 'metrics = %w[ASIS LOG EXP ROC PR MERS LOGMERS]; puts(["dataset", "motif", *metrics].join("\t")); $stdin.each_line{|l| ds, mot, info = l.chomp.split("\t"); puts([File.basename(ds), File.basename(mot), JSON.parse(info).values_at(*metrics)].join("\t")) }' > results/parsed_pbm_metrics.tsv
 cat results/selex_0.1_metrics.txt | ruby -e 'puts(["dataset", "motif", "AUCROC"].join("\t")); $stdin.each_line{|l| ds, mot, aucroc = l.chomp.split("\t"); puts([File.basename(ds), File.basename(mot), aucroc].join("\t")) }' > results/parsed_selex_0.1_metrics.tsv
 cat results/selex_0.5_metrics.txt | ruby -e 'puts(["dataset", "motif", "AUCROC"].join("\t")); $stdin.each_line{|l| ds, mot, aucroc = l.chomp.split("\t"); puts([File.basename(ds), File.basename(mot), aucroc].join("\t")) }' > results/parsed_selex_0.5_metrics.tsv
 cat results/chipseq_affiseq_metrics.txt | ruby -e 'puts(["dataset", "motif", "AUCROC"].join("\t")); $stdin.each_line{|l| ds, mot, aucroc = l.chomp.split("\t"); puts([File.basename(ds), File.basename(mot), aucroc].join("\t")) }' > results/parsed_chipseq_affiseq_metrics.tsv
