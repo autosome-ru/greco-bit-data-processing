cfd83d45b42de1d14236b7acf49af028e94fc1c0
Ilya Vorontsov <vorontsov.i.e@gmail.com>
2020-12-07 23:16:43 +0300

    added Smile-seq processing

---
+++ process_reads_HTS_SMS_AFS/process_smileseq.rb
@@ -0,0 +1,47 @@
+require 'parallel'
+require 'fileutils'
+require_relative 'fastq'
+require_relative 'train_val_split'
+
+# UT380-185_SETBP1_DBD_1_AT_hook_SS018_BC07.fastq
+def parse_filename_smileseq(filename)
+  basename = File.basename(filename, '.fastq')
+  # ['UT380-185', 'SETBP1', 'DBD', ['1', 'AT', 'hook'], 'SS018', 'BC07']
+  lab_specific_id, tf, dbd_or_fl, *domain_parts, sequencing_id, barcode_index = basename.split('_')
+  barcode_index = barcode_index.sub(/^BC0*(\d+)$/, 'BC\1') # BC07 --> BC7
+  {tf: tf, protein_structure: dbd_or_fl, domain: domain_parts.join('_')
+    barcode_index: barcode_index, sequencing_id: sequencing_id,
+    lab_specific_id: lab_specific_id, filename: filename}
+end
+
+# SmileSeq
+results_folder = "results_smileseq"
+FileUtils.mkdir_p "#{results_folder}/train_reads"
+FileUtils.mkdir_p "#{results_folder}/validation_reads"
+
+sample_filenames = Dir.glob('source_data_smileseq/smileseq_raw/*.fastq')
+
+samples = sample_filenames.map{|fn| parse_filename_smileseq(fn) }
+Parallel.each(samples, in_processes: 20) do |sample|
+  bn = sample.values_at(:tf, :protein_structure, :domain, :lab_specific_id, :sequencing_id, :barcode_index).join('.')
+  train_fn = "#{results_folder}/train_reads/#{bn}.smileseq.train.fastq"
+  validation_fn = "#{results_folder}/validation_reads/#{bn}.smileseq.val.fastq"
+  train_val_split(sample[:filename], train_fn, validation_fn)
+end
+
+File.open("#{results_folder}/stats.tsv", 'w') do |fw|
+  header = ['tf', 'protein_structure', 'domain', 'lab_specific_id', 'sequencing_id', 'barcode_index', 'train/validation', 'filename', 'num_reads']
+  fw.puts(header.join("\t"))
+  samples.each{|sample|
+    bn = sample.values_at(:tf, :protein_structure, :domain, :lab_specific_id, :sequencing_id, :barcode_index).join('.')
+    train_fn = "#{results_folder}/train_reads/#{bn}.smileseq.train.fastq"
+
+    column_infos = sample.values_at(:tf, :protein_structure, :domain, :lab_specific_id, :sequencing_id, :barcode_index)
+    info_train = [*column_infos, 'train', train_fn, num_reads_in_fastq(train_fn)]
+    fw.puts(info_train.join("\t"))
+
+    validation_fn = "#{results_folder}/validation_reads/#{bn}.smileseq.val.fastq"
+    info_validation = [*column_infos, 'validation', validation_fn, num_reads_in_fastq(validation_fn)]
+    fw.puts(info_validation.join("\t"))
+  }
+end
