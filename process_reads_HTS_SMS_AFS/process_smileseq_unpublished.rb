77b8796c4e0a2797bf1ea4da1774f5f479abe198
Ilya Vorontsov <vorontsov.i.e@gmail.com>
2021-01-05 00:51:57 +0300

    add reading unpublished smileseq metadata

--- process_reads_HTS_SMS_AFS/process_smileseq_unpublished.rb
+++ process_reads_HTS_SMS_AFS/process_smileseq_unpublished.rb
@@ -2,16 +2,7 @@ require 'parallel'
 require 'fileutils'
 require_relative 'fastq'
 require_relative 'train_val_split'
-
-module Enumerable
-  def index_by(&block)
-    each_with_object({}){|object, hsh|
-      index = block.call(object)
-      raise "Non-unique index `#{index}`"  if hsh.has_key?(index)
-      hsh[index] = object
-    }
-  end
-end
+require_relative '../shared/lib/index_by'
 
 # UT380-185_SETBP1_DBD_1_AT_hook_SS018_BC07.fastq
 def parse_filename_smileseq(filename)
@@ -19,15 +10,38 @@ def parse_filename_smileseq(filename)
   # ['UT380-185', 'SETBP1', 'DBD', ['1', 'AT', 'hook'], 'SS018', 'BC07']
   lab_specific_id, tf, dbd_or_fl, *domain_parts, sequencing_id, barcode_index = basename.split('_')
   barcode_index = barcode_index.sub(/^BC0*(\d+)$/, 'BC\1') # BC07 --> BC7
-  {tf: tf, protein_structure: dbd_or_fl, domain: domain_parts.join('_'),
-    barcode_index: barcode_index, sequencing_id: sequencing_id,
-    lab_specific_id: lab_specific_id, filename: filename}
+  {
+    tf: tf, protein_structure: dbd_or_fl, domain: domain_parts.join('_'),
+    barcode_index: barcode_index, sequencing_id: sequencing_id, lab_specific_id: lab_specific_id,
+    basename: basename, filename: filename
+  }
 end
 
-barcodes = File.readlines('source_data_smileseq/smileseq_barcode_file.txt').map{|l|
-  barcode_index, barcode_seq = l.chomp.split("\t")
-  [barcode_index, {flank_5: barcode_seq, flank_3: ''}]
-}.to_h
+def read_metadata(filename)
+  File.readlines(filename).drop(1).map{|line|
+    bbi_id, hughes_id, tf_family, ssid, barcode = line.chomp.split("\t")
+    tf, *rest = info[:hughes_id].split('.')
+    construct_type = (rest.size >= 1) ? rest[0] : 'NA'
+    {
+      tf: tf, construct_type: construct_type, unique_experiment_id: bbi_id,
+      barcode: barcode.sub(/^BC0*(\d+)$/, 'BC\1'), # BC07 --> BC7
+      hughes_id: hughes_id, tf_family: tf_family, ssid: ssid,
+    }
+  }
+end
+
+def read_barcodes(filename)
+  File.readlines(filename).map{|l|
+    barcode_index, barcode_seq = l.chomp.split("\t")
+    [barcode_index, {flank_5: barcode_seq, flank_3: ''}]
+  }.to_h
+end
+
+barcodes = read_barcodes('source_data_smileseq/unpublished/smileseq_barcode_file.txt')
+
+metadata_fn = 'source_data_smileseq/unpublished/SMiLE_seq_metadata_temp_17DEC2020_newData.tsv'
+smileseq_infos = read_metadata(metadata_fn).index_by{|info| info[:unique_experiment_id] }
+
 
 # SmileSeq
 results_folder = "results_smileseq"
@@ -38,20 +52,6 @@ sample_filenames = Dir.glob('source_data_smileseq/smileseq_raw/*.fastq')
 
 samples = sample_filenames.map{|fn| parse_filename_smileseq(fn) }
 
-smileseq_unpublished_infos = File.readlines('source_data_smileseq/SMiLE_seq_metadata_temp_17DEC2020_newData.tsv').drop(1).map{|l|
-  bbi_id, hughes_id, tf_family, ssid, barcode = l.chomp.split("\t")
-  tf, *rest = info[:hughes_id].split('.')
-  construct_type = (rest.size >= 1) ? rest[0] : 'NA'
-
-  {
-    tf: tf, construct_type: construct_type,
-    unique_experiment_id: bbi_id,
-    barcode: barcode.sub(/^BC0*(\d+)$/, 'BC\1'), # BC07 --> BC7
-    hughes_id: hughes_id,
-    tf_family: tf_family,
-    ssid: ssid,
-  }
-}.index_by{|info| info[:unique_experiment_id] }
 
 Parallel.each(samples, in_processes: 20) do |sample|
   barcode = barcodes[sample[:barcode_index]].values_at(:flank_5, :flank_3).join('+')
