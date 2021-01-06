15507ca6553e26afeef27ef5a3cac7dbd136fb2e
Ilya Vorontsov <vorontsov.i.e@gmail.com>
2021-01-06 22:51:03 +0300

    adding smileseq unpublished metadata matching

--- process_reads_HTS_SMS_AFS/process_smileseq_unpublished.rb
+++ process_reads_HTS_SMS_AFS/process_smileseq_unpublished.rb
@@ -19,11 +19,10 @@ module SMSUnpublished
   end
 
   class DatasetNaming
-    attr_reader :results_folder, :barcodes, :metadata
-    def initialize(results_folder, barcodes:, metadata:)
+    attr_reader :results_folder, :barcodes
+    def initialize(results_folder, barcodes:)
       @results_folder = results_folder
       @barcodes = barcodes
-      @metadata = metadata
     end
 
     def self.create_folders!
@@ -33,10 +32,6 @@ module SMSUnpublished
 
     def basename(sample)
       experiment_id = sample.experiment_id
-      sample_metadata = metadata[experiment_id]
-      unless sample.barcode_index == sample_metadata.barcode_index
-        raise "Metadata for `#{experiment_id}` contradicts data encoded in filename `#{sample.filename}`"
-      end
       barcode = barcodes[barcode_index]
       flank_5 = (ADAPTER_5 + barcode[:flank_5])[-20,20]
       flank_3 = (barcode[:flank_3] + ADAPTER_3)[0,20]
@@ -49,7 +44,7 @@ module SMSUnpublished
     def stats_filename; "#{results_folder}/stats.tsv"; end
   end
 
-  Sample = Struct.new(*[:experiment_id, :tf, :construct_type, :domain, :barcode_index, :sequencing_id, :filename], keyword_init: true) do
+  Sample = Struct.new(*[:experiment_id, :tf, :construct_type, :barcode_index, :domain, :sequencing_id, :filename], keyword_init: true) do
     # UT380-185_SETBP1_DBD_1_AT_hook_SS018_BC07.fastq
     def self.from_filename(filename)
       basename = File.basename(filename, '.fastq')
@@ -58,13 +53,13 @@ module SMSUnpublished
       experiment_id = experiment_id_match[1] + '-' + experiment_id_match[2] # UT380_501 --> UT380-501, UT380408 --> UT380-408
       tf, dbd_or_fl, *domain_parts, sequencing_id, barcode_index = basename[experiment_id_match[0].length..-1].split('_')
       barcode_index = barcode_index.sub(/^BC0*(\d+)$/, 'BC\1') # BC07 --> BC7
-      self.new(tf: tf, construct_type: dbd_or_fl, domain: domain_parts.join('_'),
-        barcode_index: barcode_index, sequencing_id: sequencing_id, experiment_id: experiment_id,
+      self.new(experiment_id: experiment_id, tf: tf, construct_type: dbd_or_fl,
+        barcode_index: barcode_index, domain: domain_parts.join('_'), sequencing_id: sequencing_id,
         filename: filename)
     end
   end
 
-  SampleMetadata = Struct.new(*[:tf, :construct_type, :experiment_id, :barcode_index, :hughes_id, :tf_family, :ssid], keyword_init: true) do
+  SampleMetadata = Struct.new(*[:experiment_id, :tf, :construct_type, :barcode_index, :hughes_id, :tf_family, :ssid], keyword_init: true) do
     def self.from_string(line)
       # Example:
       ## BBI_ID  Hughes_ID TF_family SSID  Barcode
@@ -72,7 +67,7 @@ module SMSUnpublished
       bbi_id, hughes_id, tf_family, ssid, barcode_index = line.chomp.split("\t")
       tf, *rest = hughes_id.split('.')  # hughes_id examples: `MBD4`, `BHLHA9.FL`, `CASZ1.DBD.1`
       construct_type = (rest.size >= 1) ? rest[0] : 'NA'
-      self.new(tf: tf, construct_type: construct_type, experiment_id: bbi_id,
+      self.new(experiment_id: bbi_id, tf: tf, construct_type: construct_type,
         barcode_index: barcode_index.sub(/^BC0*(\d+)$/, 'BC\1'), # BC07 --> BC7
         hughes_id: hughes_id, tf_family: tf_family, ssid: ssid)
     end
@@ -83,6 +78,11 @@ module SMSUnpublished
         yield self.from_string(line)
       }
     end
+  end
+
+  def self.match_metadata?(sample, sample_metadata)
+    fields = [:experiment_id, :tf, :construct_type, :barcode_index]
+    sample.to_h.values_at(*fields) == sample_metadata.to_h.values_at(*fields)
   end
 end
 
@@ -108,13 +108,26 @@ end
 SOURCE_FOLDER = 'source_data_smileseq/unpublished'
 barcodes = SMSUnpublished.read_barcodes("#{SOURCE_FOLDER}/smileseq_barcode_file.txt")
 
-metadata_fn = "#{SOURCE_FOLDER}/SMiLE_seq_metadata_temp_17DEC2020_newData.tsv"
-smileseq_infos = SMSUnpublished::SampleMetadata.each_in_file(metadata_fn).index_by(&:experiment_id)
-
 sample_filenames = Dir.glob("#{SOURCE_FOLDER}/smileseq_raw/*.fastq")
 samples = sample_filenames.map{|fn| SMSUnpublished::Sample.from_filename(fn) }
 
-ds_naming = SMSUnpublished::DatasetNaming.new("results_smileseq", barcodes: barcodes, metadata: smileseq_infos)
+metadata_fn = "#{SOURCE_FOLDER}/SMiLE_seq_metadata_temp_17DEC2020_newData.tsv"
+metadata = SMSUnpublished::SampleMetadata.each_in_file(metadata_fn).to_a
+
+metadata_by_experiment_id = metadata.index_by(&:experiment_id)
+sample_by_experiment_id = samples.index_by(&:experiment_id)
+
+sample_meta_pairs = []
+sample_meta_pairs += samples.map{|sample|  [sample, metadata_by_experiment_id[sample.experiment_id]]  }
+sample_meta_pairs += metadata.map{|sample_metadata|  [sample_by_experiment_id[sample_metadata.experiment_id], sample_metadata]  }
+
+sample_meta_pairs.each{|sample, sample_metadata|
+  unless SMSUnpublished.match_metadata?(sample, sample_metadata)
+    raise "Metadata for #{sample.experiment_id} doesn't match info in filename"
+  end
+}
+
+ds_naming = SMSUnpublished::DatasetNaming.new("results_smileseq", barcodes: barcodes, metadata: metadata)
 ds_naming.create_folders!
 
 Parallel.each(samples, in_processes: 20) do |sample|
