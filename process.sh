437e65f6eb7c5ace37c6700afc228e7edbcd07cd
Ilya Vorontsov <vorontsov.i.e@gmail.com>
2021-01-22 17:42:01 +0300

    add clustering

--- process.sh
+++ process.sh
@@ -1,2 +1,6 @@
 # Run once to generate a pool of unique random names
 python3 shared/lib/random_names.py
+
+# Clustering of PCMs/PPMs
+#java -jar clustering/MotifClustering-0.1.jar p=pcms_folder outdir=tree_folder
+
