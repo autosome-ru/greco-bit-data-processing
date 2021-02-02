530ae0668b10f5ecd4f715ff9982da47de8f009e
Ilya Vorontsov <vorontsov.i.e@gmail.com>
2021-02-02 19:23:21 +0300

    fixes

--- shared/lib/random_names.rb
+++ shared/lib/random_names.rb
@@ -3,7 +3,7 @@ require "sqlite3"
 # Open a database
 $dataset_names_DB = SQLite3::Database.new('dataset_names.db')
 def take_dataset_name!
-  while True
+  while true
     begin
       ds_id, ds_name = $dataset_names_DB.execute("SELECT id, name FROM dataset_names WHERE status == 'not-used' LIMIT 1").first
       $dataset_names_DB.execute("UPDATE dataset_names SET status = 'used' WHERE id == (?)", [ds_id])
