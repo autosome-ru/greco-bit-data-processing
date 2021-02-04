0b61608344722910dac8d10cdc0be9aa904f9f3e
Ilya Vorontsov <vorontsov.i.e@gmail.com>
2021-02-04 10:17:04 +0300

    refactor

--- process_reads_HTS_SMS_AFS/reads_processing.rb
+++ process_reads_HTS_SMS_AFS/reads_processing.rb
@@ -3,10 +3,21 @@ require 'fileutils'
 require_relative 'train_val_split'
 require_relative 'dataset_naming'
 
+def left_join_by(collection_1, collection_2, key_proc_1: nil, key_proc_2: nil, &key_proc)
+  key_proc_1 ||= key_proc
+  key_proc_2 ||= key_proc
+  collection_1_by_key = collection_1.index_by(&key_proc_1)
+  collection_2_by_key = collection_2.index_by(&key_proc_2)
+  collection_1_by_key.map{|obj_1, key|
+    obj_2 = collection_2_by_key[key]
+    [key, obj_1, obj_2]
+  }
+end
+
 module ReadsProcessing
   # processing_module = SMSPublished / SMSUnpublished
   def self.process!(processing_module, results_folder, samples, metadata, barcode_proc, num_threads: 1)
-    sample_triples = ReadsProcessing.collect_sample_triples(samples, metadata)
+    sample_triples = left_join_by(samples, metadata, &:experiment_id)
     verify_sample_triples!(processing_module, sample_triples)
 
     ds_naming = ReadsProcessing::DatasetNaming.new(results_folder, barcode_proc: barcode_proc, metadata: metadata)
@@ -19,16 +30,6 @@ module ReadsProcessing
     end
 
     ReadsProcessing.generate_samples_stats(processing_module::SampleMetadata, ds_naming, sample_triples)
-  end
-
-  def self.collect_sample_triples(samples, metadata)
-    metadata_by_experiment_id = metadata.index_by(&:experiment_id)
-    sample_by_experiment_id = samples.index_by(&:experiment_id)
-
-    sample_triples = sample_by_experiment_id.map{|experiment_id, sample|
-      sample_metadata = metadata_by_experiment_id[experiment_id]
-      [experiment_id, sample, sample_metadata]
-    }
   end
 
   # matcher is any object with `match_metadata?` method. E.g. SMSPublished / SMSUnpublished
