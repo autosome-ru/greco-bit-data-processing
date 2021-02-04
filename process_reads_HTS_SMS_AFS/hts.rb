e07349d092c75c3ef4bedd0c61e7b4b63a35628d
Ilya Vorontsov <vorontsov.i.e@gmail.com>
2021-02-04 11:43:22 +0300

    rename adaptor -> barcode; change fields to validate between HTS samples and metadata

--- process_reads_HTS_SMS_AFS/hts.rb
+++ process_reads_HTS_SMS_AFS/hts.rb
@@ -15,27 +15,30 @@ module Selex
     # }.to_h
   end
 
-  # adapter_str example: 'GG40NCGTAGT'
-  def self.parse_adapter(adapter_str)
-    adapter_match = adapter_str.match(/^(?<flank_5>[ACGT]+)(?<insertion_length>\d+)N(?<flank_3>[ACGT]+)$/)
+  # barcode_str example: 'GG40NCGTAGT'
+  def self.parse_barcode(barcode_str)
+    barcode_match = barcode_str.match(/^(?<flank_5>[ACGT]+)(?<insertion_length>\d+)N(?<flank_3>[ACGT]+)$/)
     {
-      flank_5: adapter_match[:flank_5],
-      flank_3: adapter_match[:flank_3],
-      insertion_length: Integer(adapter_match[:insertion_length]),
+      flank_5: barcode_match[:flank_5],
+      flank_3: barcode_match[:flank_3],
+      insertion_length: Integer(barcode_match[:insertion_length]),
     }
   end
 
-  Sample = Struct.new(*[:tf, :adapter, :experiment_subtype, :batch, :cycle, :filename], keyword_init: true) do
+  Sample = Struct.new(*[:tf, :barcode, :experiment_subtype, :batch, :cycle, :filename], keyword_init: true) do
 
     # AHCTF1_GG40NCGTAGT_IVT_BatchYWCB_Cycle3_R1.fastq.gz
     # SNAI1_AC40NGCTGCT_Lysate_BatchAATA_Cycle2_R1.fastq.gz
     def self.from_filename(filename)
       basename = File.basename(File.basename(filename, '.gz'), '.fastq')
-      tf, adapter, experiment_subtype, batch, cycle, reads_part = basename.split('_')
+      tf, barcode_str, experiment_subtype, batch, cycle, reads_part = basename.split('_')
       raise  unless reads_part == 'R1'
       raise  unless batch.start_with?('Batch')
-      self.new(tf: tf, experiment_subtype: experiment_subtype, cycle: cycle,
-        adapter: Selex.parse_adapter(adapter), batch: batch.sub(/^Batch/, ''),
+      raise  unless cycle.start_with?('Cycle')
+      self.new(tf: tf, experiment_subtype: experiment_subtype,
+        cycle: Integer(cycle.sub(/^Cycle/, '')),
+        barcode: Selex.parse_barcode(barcode_str),
+        batch: batch.sub(/^Batch/, ''),
         filename: filename)
     end
   end
@@ -62,7 +65,7 @@ module Selex
     end
 
     def barcode
-      Selex.parse_adapter( self.dna_library_id.split('_').first )
+      Selex.parse_barcode( self.dna_library_id.split('_').first )
     end
 
     def experiment_type; "HTS_#{experiment_subtype}"; end
@@ -77,8 +80,8 @@ module Selex
       raise "Unknown experiment subtype `#{experiment_subtype}`"  unless ['IVT', 'Lysate'].include?(experiment_subtype)
       experiment_subtype = experiment_subtype[0,3]
       raise  unless dna_library_id.match?(/^[ACGT]+\d+N[ACGT]+_v1$/)
-      # adapter_str = dna_library_id.sub(/_v1$/, '') # GT40NGCGTGT_v1 --> GT40NGCGTGT
-      # adapter = Selex.parse_adapter(adapter_str)
+      # barcode_str = dna_library_id.sub(/_v1$/, '') # GT40NGCGTGT_v1 --> GT40NGCGTGT
+      # barcode = Selex.parse_barcode(barcode_str)
 
       filename_or_nil = ->(fn){ (fn == 'No_Data') ? nil : fn }
       self.new(
@@ -102,45 +105,7 @@ module Selex
   end
 
   def self.match_metadata?(sample, sample_metadata)
-    fields = [:experiment_id, :tf, :construct_type, :barcode_index]
+    fields = [:tf, :experiment_subtype, :barcode]
     sample.to_h.values_at(*fields) == sample_metadata.to_h.values_at(*fields)
   end
 end
-
-# # Selex::SampleMetadata.each_in_file
-
-
-# # # SELEX (and without AffiSeq!)
-# # ['IVT', 'Lysate'].each{|experiment_type|
-# #   results_folder = "results_#{experiment_type}"
-# #   FileUtils.mkdir_p "#{results_folder}/train_reads"
-# #   FileUtils.mkdir_p "#{results_folder}/validation_reads"
-
-# #   sample_filenames = Dir.glob('source_data/reads/*.fastq.gz')
-# #   sample_filenames.select!{|fn| File.basename(fn).match?(/_#{experiment_type}_/) }
-# #   sample_filenames.reject!{|fn| File.basename(fn).match?(/_AffSeq_/) }
-
-# #   samples = sample_filenames.map{|fn| parse_filename_selex(fn) }
-# #   Parallel.each(samples, in_processes: 20) do |sample|
-# #     # In SELEX there are no paired reads, so we don't add it to filename
-# #     bn = sample.values_at(:tf, :type, :cycle, :adapter, :batch).join('.')
-# #     train_fn = "#{results_folder}/train_reads/#{bn}.selex.train.fastq.gz"
-# #     validation_fn = "#{results_folder}/validation_reads/#{bn}.selex.val.fastq.gz"
-# #     train_val_split(sample[:filename], train_fn, validation_fn)
-# #   end
-
-# #   File.open("#{results_folder}/stats.tsv", 'w') do |fw|
-# #     header = ['tf', 'type', 'cycle', 'adapter', 'batch', 'train/validation', 'filename', 'num_reads']
-# #     fw.puts(header.join("\t"))
-# #     samples.each{|sample|
-# #       bn = sample.values_at(:tf, :type, :cycle, :adapter, :batch).join('.')
-# #       train_fn = "#{results_folder}/train_reads/#{bn}.selex.train.fastq.gz"
-# #       info_train = sample.values_at(:tf, :type, :cycle, :adapter, :batch) + ['train', train_fn, num_reads_in_fastq(train_fn)]
-# #       fw.puts(info_train.join("\t"))
-
-# #       validation_fn = "#{results_folder}/validation_reads/#{bn}.selex.val.fastq.gz"
-# #       info_validation = sample.values_at(:tf, :type, :cycle, :adapter, :batch) + ['validation', validation_fn, num_reads_in_fastq(validation_fn)]
-# #       fw.puts(info_validation.join("\t"))
-# #     }
-# #   end
-# # }
