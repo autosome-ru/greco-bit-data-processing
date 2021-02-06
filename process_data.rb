4ee40d1b68f815814750265eae624b2f5c2d5b60
Ilya Vorontsov <vorontsov.i.e@gmail.com>
2021-02-06 22:40:11 +0300

    rename folders for SMiLE-seq

--- process_data.rb
+++ process_data.rb
@@ -11,7 +11,7 @@ def process_sms_unpublished!
   metadata_fn = "source_data_meta/SMS/unpublished/SMiLE_seq_metadata_temp_17DEC2020_newData.tsv"
   barcodes_fn = "source_data_meta/SMS/unpublished/smileseq_barcode_file.txt"
   samples_glob = "source_data/SMS/reads/unpublished/*.fastq"
-  results_folder = "source_data_prepared/SMS/unpublished/reads/"
+  results_folder = "source_data_prepared/SMS/reads/"
 
   barcodes = SMSUnpublished.read_barcodes(barcodes_fn)
   barcode_proc = ->(sample_metadata){ barcodes[sample_metadata.barcode_index] }
@@ -32,13 +32,14 @@ def process_sms_published!
   metadata_fn = "source_data_meta/SMS/published/SMS_published.tsv"
   barcodes_fn = "source_data_meta/SMS/published/Barcode_sequences.txt"
   samples_glob = "source_data/SMS/reads/published/*.fastq"
-  results_folder = "source_data_prepared/SMS/published/reads/"
+  results_folder = "source_data_prepared/SMS.published/reads/"
 
   barcodes = SMSPublished.read_barcodes(barcodes_fn)
   barcode_proc = ->(sample_metadata){ barcodes[sample_metadata.barcode_index] }
 
   samples = Dir.glob(samples_glob).map{|fn| SMSPublished::Sample.from_filename(fn) }
   metadata = SMSPublished::SampleMetadata.each_in_file(metadata_fn).to_a
+  metadata = metadata.reject{|m| m.tfs.size != 1 }
   samples = unique_samples(samples)
   metadata = unique_metadata(metadata)
 
