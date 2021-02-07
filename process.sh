32a5233ed75f48a6b2e9f9527b67e070b10f3f95
Ilya Vorontsov <vorontsov.i.e@gmail.com>
2021-02-08 01:16:16 +0300

    how to run

--- process.sh
+++ process.sh
@@ -4,3 +4,5 @@ python3 shared/lib/random_names.py
 # Clustering of PCMs/PPMs
 #java -jar clustering/MotifClustering-0.1.jar p=pcms_folder outdir=tree_folder
 
+./process_PBM/process_data.sh --source source_data/PBM/chips/ --name-mapping no
+ruby ./process_data.rb
