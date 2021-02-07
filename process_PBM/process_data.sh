0d163e0baceb76c0b08ccb1f48e8cba5b027a002
Ilya Vorontsov <vorontsov.i.e@gmail.com>
2021-02-07 20:29:33 +0300

    PBM data processing configuration

--- process_PBM/process_data.sh
+++ process_PBM/process_data.sh
@@ -5,18 +5,57 @@ CHIPS_SOURCE_FOLDER=./data/RawData
 NUM_THREADS=20
 NORMALIZATION_OPTS='--log10'
 
-
 INTERMEDIATE_FOLDER='results_databox_intermediate'
 RESULTS_FOLDER='results_databox'
 
-ruby rename_chips.rb \
-     --source ${CHIPS_SOURCE_FOLDER} \
-     --destination ${INTERMEDIATE_FOLDER}/raw_intensities/ \
-     --tf-mapping 'tf_name_mapping.txt'
+NAME_MAPPING='tf_name_mapping.txt' # use `--name_mapping no` to skip mapping
+
+while true; do
+    case "${1-}" in
+        --source)
+            CHIPS_SOURCE_FOLDER="$(readlink -m "$2")"
+            shift
+            ;;
+        --destination)
+            RESULTS_FOLDER="$(readlink -m "$2")"
+            shift
+            ;;
+        --tmp)
+            INTERMEDIATE_FOLDER="$(readlink -m "$2")"
+            shift
+            ;;
+        --num-threads)
+            NUM_THREADS="$2"
+            shift
+            ;;
+        --name-mapping)
+            if [[ "$2" == "no" ]]; then
+                NAME_MAPPING="no"
+            else
+                NAME_MAPPING="$(readlink -m "$2")"
+            fi
+            shift
+            ;;
+        -?*)
+            echo -e "WARN: Unknown option (ignored): $1\n" >&2
+            ;;
+        *)
+            break
+    esac
+    shift
+done
+
+if [[ "$NAME_MAPPING" == "no" ]]; then
+    ruby rename_chips.rb \
+         --source ${CHIPS_SOURCE_FOLDER} \
+         --destination ${INTERMEDIATE_FOLDER}/raw_intensities/ \
+         --tf-mapping "${NAME_MAPPING}";
+    CHIPS_SOURCE_FOLDER="${INTERMEDIATE_FOLDER}/raw_intensities/"
+fi
 
 ## sd_qn_intensities
 # window-size=5 means window 11x11
-./spatial_detrending.sh --source ${INTERMEDIATE_FOLDER}/raw_intensities/ \
+./spatial_detrending.sh --source ${CHIPS_SOURCE_FOLDER} \
                         --destination ${INTERMEDIATE_FOLDER}/spatial_detrended_intensities/ \
                         --window-size 5 \
                         --num-threads ${NUM_THREADS}
@@ -29,7 +68,7 @@ ruby quantile_normalize_chips.rb \
 ## qn_zscore_intensities
 ruby quantile_normalize_chips.rb \
         ${NORMALIZATION_OPTS} \
-        --source ${INTERMEDIATE_FOLDER}/raw_intensities/ \
+        --source ${CHIPS_SOURCE_FOLDER} \
         --destination ${INTERMEDIATE_FOLDER}/quantile_normalized_intensities
 
 ruby zscore_transform_chips.rb \
@@ -43,12 +82,12 @@ mkdir -p ${RESULTS_FOLDER}/spatialDetrend_quantNorm/train_intensities  ${RESULTS
 mkdir -p ${RESULTS_FOLDER}/quantNorm_zscore/train_intensities  ${RESULTS_FOLDER}/quantNorm_zscore/validation_intensities
 
 # raw
-for FN in $(find ${INTERMEDIATE_FOLDER}/raw_intensities/ -xtype f -name '*_1M-ME_*'); do
+for FN in $(find ${CHIPS_SOURCE_FOLDER} -xtype f -name '*_1M-ME_*'); do
     BN=$(basename -s .pbm.txt ${FN})
     cp ${FN} ${RESULTS_FOLDER}/raw/train_intensities/${BN}.raw.pbm.train.txt
 done
 
-for FN in $(find ${INTERMEDIATE_FOLDER}/raw_intensities/ -xtype f -name '*_1M-HK_*'); do
+for FN in $(find ${CHIPS_SOURCE_FOLDER} -xtype f -name '*_1M-HK_*'); do
     BN=$(basename -s .pbm.txt ${FN})
     cp ${FN} ${RESULTS_FOLDER}/raw/validation_intensities/${BN}.raw.pbm.val.txt
 done
