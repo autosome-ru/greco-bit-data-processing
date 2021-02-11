18e4bce064550715b841524b41fb0f26aaff543c
Ilya Vorontsov <vorontsov.i.e@gmail.com>
2021-02-11 18:44:13 +0300

    AFS peaks processing fixes

--- process_data_AFS.sh
+++ process_data_AFS.sh
@@ -1,44 +1,52 @@
 SOURCE_FOLDER=./source_data/AFS/
-RESULTS_FOLDER=./results_databox_afs/
 SCRIPT_FOLDER=./process_peaks_CHS_AFS/
 
-mkdir -p "${RESULTS_FOLDER}"
-
-ruby "${SCRIPT_FOLDER}/prepare_peaks_affiseq.rb" "${SOURCE_FOLDER}" "${RESULTS_FOLDER}"
-
-mkdir -p ${RESULTS_FOLDER}/Train_sequences/
-for FN in $(find ${RESULTS_FOLDER}/Train_intervals/ -xtype f); do
-  TF="$(basename -s .interval "$FN")"
-  cat ${FN} | tail -n+2 | sort -k5,5nr | head -500 | ./bedtools getfasta -fi ./source_data/hg38.fa -bed - > ${RESULTS_FOLDER}/Train_sequences/${TF}.fa
+# It's essential that IVT and Lysate datasets are stored into different intermediate folders: they can conflict
+for EXP_TYPE in IVT Lysate; do
+  RESULTS_FOLDER=./results_databox_afs_${EXP_TYPE}/
+  mkdir -p "${RESULTS_FOLDER}"
+  ruby "${SCRIPT_FOLDER}/prepare_peaks_affiseq.rb" "${SOURCE_FOLDER}" "${RESULTS_FOLDER}" --experiment-type ${EXP_TYPE}
 done
 
-mkdir -p ${RESULTS_FOLDER}/Val_sequences/
-for FN in $(find ${RESULTS_FOLDER}/Val_intervals/ -xtype f); do
-  TF="$(basename -s .interval "$FN")"
-  cat ${FN} | tail -n+2 | sort -k5,5nr | head -500 | ./bedtools getfasta -fi ./source_data/hg38.fa -bed - > ${RESULTS_FOLDER}/Val_sequences/${TF}.fa
-done
-
-for SLICE_TYPE in Train Val; do
-  mkdir -p source_data_prepared/AFS/${SLICE_TYPE}_intervals
-  mkdir -p source_data_prepared/AFS/${SLICE_TYPE}_sequences
-
-  for FN in $(find ${RESULTS_FOLDER}/${SLICE_TYPE}_intervals/ -xtype f ); do
-      # BN=$(basename -s .pbm.txt ${FN})
-      NEW_BN=$( ruby ${SCRIPT_FOLDER}/name_sample_afs.rb "$FN" --slice-type ${SLICE_TYPE} --extension peaks )
-      if [[ -n "$NEW_BN" ]]; then
-          cp ${FN} source_data_prepared/AFS/${SLICE_TYPE}_intervals/${NEW_BN}
-      else
-          echo "Can't get filename for ${FN}. Probably no metadata supplied" >& 2
-      fi
+for EXP_TYPE in IVT Lysate; do
+  RESULTS_FOLDER=./results_databox_afs_${EXP_TYPE}/
+  mkdir -p ${RESULTS_FOLDER}/Train_sequences/
+  for FN in $(find ${RESULTS_FOLDER}/Train_intervals/ -xtype f); do
+    TF="$(basename -s .interval "$FN")"
+    cat ${FN} | tail -n+2 | sort -k5,5nr | head -500 | ./bedtools getfasta -fi ./source_data/hg38.fa -bed - > ${RESULTS_FOLDER}/Train_sequences/${TF}.fa
   done
 
-  for FN in $(find ${RESULTS_FOLDER}/${SLICE_TYPE}_sequences/ -xtype f ); do
-      # BN=$(basename -s .pbm.txt ${FN})
-      NEW_BN=$( ruby ${SCRIPT_FOLDER}/name_sample_afs.rb "$FN" --slice-type ${SLICE_TYPE} --extension fa )
-      if [[ -n "$NEW_BN" ]]; then
-          cp ${FN} source_data_prepared/AFS/${SLICE_TYPE}_sequences/${NEW_BN}
-      else
-          echo "Can't get filename for ${FN}. Probably no metadata supplied" >& 2
-      fi
+  mkdir -p ${RESULTS_FOLDER}/Val_sequences/
+  for FN in $(find ${RESULTS_FOLDER}/Val_intervals/ -xtype f); do
+    TF="$(basename -s .interval "$FN")"
+    cat ${FN} | tail -n+2 | sort -k5,5nr | head -500 | ./bedtools getfasta -fi ./source_data/hg38.fa -bed - > ${RESULTS_FOLDER}/Val_sequences/${TF}.fa
+  done
+done
+
+for EXP_TYPE in IVT Lysate; do
+  RESULTS_FOLDER=./results_databox_afs_${EXP_TYPE}/
+  for SLICE_TYPE in Train Val; do
+    mkdir -p source_data_prepared/AFS/${SLICE_TYPE}_intervals
+    mkdir -p source_data_prepared/AFS/${SLICE_TYPE}_sequences
+
+    for FN in $(find ${RESULTS_FOLDER}/${SLICE_TYPE}_intervals/ -xtype f ); do
+        # BN=$(basename -s .pbm.txt ${FN})
+        NEW_BN=$( ruby ${SCRIPT_FOLDER}/name_sample_afs.rb "$FN" --slice-type ${SLICE_TYPE} --extension peaks )
+        if [[ -n "$NEW_BN" ]]; then
+            cp ${FN} source_data_prepared/AFS/${SLICE_TYPE}_intervals/${NEW_BN}
+        else
+            echo "Can't get filename for ${FN}. Probably no metadata supplied" >& 2
+        fi
+    done
+
+    for FN in $(find ${RESULTS_FOLDER}/${SLICE_TYPE}_sequences/ -xtype f ); do
+        # BN=$(basename -s .pbm.txt ${FN})
+        NEW_BN=$( ruby ${SCRIPT_FOLDER}/name_sample_afs.rb "$FN" --slice-type ${SLICE_TYPE} --extension fa )
+        if [[ -n "$NEW_BN" ]]; then
+            cp ${FN} source_data_prepared/AFS/${SLICE_TYPE}_sequences/${NEW_BN}
+        else
+            echo "Can't get filename for ${FN}. Probably no metadata supplied" >& 2
+        fi
+    done
   done
 done
