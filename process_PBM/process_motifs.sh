f2ee43e36ae894723be2a16ed6a719426caf31d7
Ilya Vorontsov <vorontsov.i.e@gmail.com>
2021-03-11 13:20:11 +0300

    process motifs for PBM refactor

--- process_PBM/process_motifs.sh
+++ process_PBM/process_motifs.sh
@@ -14,16 +14,13 @@ set -euo pipefail
 #
 
 ###########3
-# CHIPS_SOURCE_FOLDER=./data/RawData
-# RESULTS_FOLDER=./results
+mkdir -p ./release_6_motifs/Train_intensities/
 
-# CHIPS_SOURCE_FOLDER='./release_2_motifs/'
-# RESULTS_FOLDER='./release_2_motifs/results_top1000_15-8_single_log_simple_flag/'
+cp ~/greco-data/release_6.2021-02-13/PBM.SDQN/Train_intensities/* ./release_6_motifs/Train_intensities/
+cp ~/greco-data/release_6.2021-02-13/PBM.QNZS/Train_intensities/* ./release_6_motifs/Train_intensities/
 
-# cp ~/greco-data/release_4.2020-11-26/pbm/quantNorm_zscore/ ./release_4_motifs/ -r
-# cp ~/greco-data/release_4.2020-11-26/pbm/spatialDetrend_quantNorm/ ./release_4_motifs/ -r
-CHIPS_SOURCE_FOLDER='./release_4_motifs/'
-RESULTS_FOLDER='./release_4_motifs/'
+CHIPS_SOURCE_FOLDER='./release_6_motifs/'
+RESULTS_FOLDER='./release_6_motifs/'
 #######
 
 # TOP_OPTS='--max-head-size 1000'
@@ -96,59 +93,30 @@ while true; do
     shift
 done
 
-mkdir -p ./resulting_pcms
-
-SUBFOLDERS="spatialDetrend_quantNorm  quantNorm_zscore"
-
-for SUBFOLDER in $SUBFOLDERS; do
-    ruby chip_sequences.rb \
-            --source ${CHIPS_SOURCE_FOLDER}/${SUBFOLDER}/train_intensities \
-            --destination ${RESULTS_FOLDER}/${SUBFOLDER}/train_sequences \
-            --linker-length 0 \
-            --fasta  --take-top 1000
+ruby chip_sequences.rb \
+        --source ${CHIPS_SOURCE_FOLDER}/Train_intensities \
+        --destination ${RESULTS_FOLDER}/Train_sequences \
+        --linker-length 0 \
+        --fasta  --take-top 1000
 
 
-    ./calculate_motifs.sh --source ${RESULTS_FOLDER}/${SUBFOLDER}/train_sequences \
-                      --results-destination ${RESULTS_FOLDER}/${SUBFOLDER}/chipmunk_results \
-                      --logs-destination ${RESULTS_FOLDER}/${SUBFOLDER}/chipmunk_logs \
-                      --num-inner-threads ${CHIPMUNK_NUM_INNER_THREADS} \
-                      --num-processes ${CHIPMUNK_NUM_PROCESSES} \
-                      --length-range ${CHIPMUNK_LENGTH_RANGE} \
-                      --shape ${CHIPMUNK_SHAPE} \
-                      --weighting-mode ${CHIPMUNK_WEIGHTING_MODE} \
-                      --additional-options "${CHIPMUNK_ADDITIONAL_OPTIONS}"
+./calculate_motifs.sh --source ${RESULTS_FOLDER}/Train_sequences \
+                  --results-destination ${RESULTS_FOLDER}/chipmunk_results \
+                  --logs-destination ${RESULTS_FOLDER}/chipmunk_logs \
+                  --num-inner-threads ${CHIPMUNK_NUM_INNER_THREADS} \
+                  --num-processes ${CHIPMUNK_NUM_PROCESSES} \
+                  --length-range ${CHIPMUNK_LENGTH_RANGE} \
+                  --shape ${CHIPMUNK_SHAPE} \
+                  --weighting-mode ${CHIPMUNK_WEIGHTING_MODE} \
+                  --additional-options "${CHIPMUNK_ADDITIONAL_OPTIONS}"
 
-    ./extract_pcms.sh --source ${RESULTS_FOLDER}/${SUBFOLDER}/chipmunk_results \
-                  --pcms-destination ${RESULTS_FOLDER}/${SUBFOLDER}/pcms \
-                  --dpcms-destination ${RESULTS_FOLDER}/${SUBFOLDER}/dpcms \
-                  --words-destination ${RESULTS_FOLDER}/${SUBFOLDER}/words \
-                  --suffix .chipmunk.model1
+./extract_pcms.sh --source ${RESULTS_FOLDER}/chipmunk_results \
+              --pcms-destination ${RESULTS_FOLDER}/pcms \
+              --dpcms-destination ${RESULTS_FOLDER}/dpcms \
+              --words-destination ${RESULTS_FOLDER}/words \
+              --suffix .chipmunk.model1
 
-    ./generate_logo.sh --source ${RESULTS_FOLDER}/${SUBFOLDER}/pcms --destination ${RESULTS_FOLDER}/${SUBFOLDER}/logo
-    ./generate_dilogo.sh --source ${RESULTS_FOLDER}/${SUBFOLDER}/dpcms --destination ${RESULTS_FOLDER}/${SUBFOLDER}/dilogo
+./generate_logo.sh --source ${RESULTS_FOLDER}/pcms --destination ${RESULTS_FOLDER}/logo
+./generate_dilogo.sh --source ${RESULTS_FOLDER}/dpcms --destination ${RESULTS_FOLDER}/dilogo
 
-    ./convert_pcm2pfm.sh  --source ${RESULTS_FOLDER}/${SUBFOLDER}/pcms --destination ${RESULTS_FOLDER}/${SUBFOLDER}/pfms
-
-    cp ${RESULTS_FOLDER}/${SUBFOLDER}/pcms/* ./resulting_pcms
-done
-
-ruby generate_short_summary.rb  $( for SF in $SUBFOLDERS; do echo -n --logo-source  ${RESULTS_FOLDER}/${SF}/logo " "; done ) \
-                                --html-destination ${RESULTS_FOLDER}/results.html \
-                                --tsv-destination ${RESULTS_FOLDER}/results.tsv \
-                                --web-sources-url ../websrc
-
-
-#for SUBFOLDER in spatialDetrend_quantNorm  quantNorm_zscore; do
-#    for METRICS in ASIS EXP LOG ROC PR ROCLOG PRLOG ; do
-#    mkdir -p ${RESULTS_FOLDER}/${SUBFOLDER}/motif_metrics
-#        echo "./motif_metrics.sh  " \
-#                "--metrics ${METRICS}" \
-#                "--linker-opts '--linker-length 6'" \
-#                "--chips-source ${RESULTS_FOLDER}/${SUBFOLDER}/" \
-#                "--motifs-source ${RESULTS_FOLDER}/pfms" \
-#        "> ${RESULTS_FOLDER}/${SUBFOLDER}/motif_metrics/motif_metrics_${METRICS}.tsv"
-#    done
-#done | parallel -j ${NUM_THREADS}
-
-
-#ruby organize_results.rb --chips-source ${RESULTS_FOLDER}/raw_chips/ --results-source ${RESULTS_FOLDER} --destination ${RESULTS_FOLDER}/factors
+./convert_pcm2pfm.sh  --source ${RESULTS_FOLDER}/pcms --destination ${RESULTS_FOLDER}/pfms
