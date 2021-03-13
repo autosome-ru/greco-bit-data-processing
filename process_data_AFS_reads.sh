9d5ff1ec6c3d13b231d55a3c2ab4ce68ef72d286
Ilya Vorontsov <vorontsov.i.e@gmail.com>
2021-03-13 13:09:51 +0300

    fix AFS.reads extension

--- process_data_AFS_reads.sh
+++ process_data_AFS_reads.sh
@@ -9,7 +9,7 @@ for EXP_TYPE in IVT Lysate; do
 
     for FN in $(find ${RESULTS_FOLDER}/${SLICE_TYPE}_sequences/ -xtype f ); do
         # BN=$(basename -s .pbm.txt ${FN})
-        NEW_BN=$( ruby shared/bin/name_sample_afs.rb "$FN" --processing-type Reads --slice-type ${SLICE_TYPE} --extension fa )
+        NEW_BN=$( ruby shared/bin/name_sample_afs.rb "$FN" --processing-type Reads --slice-type ${SLICE_TYPE} --extension fastq.gz )
         if [[ -n "$NEW_BN" ]]; then
             cp ${FN} source_data_prepared/AFS.Reads/${SLICE_TYPE}_sequences/${NEW_BN}
         else
