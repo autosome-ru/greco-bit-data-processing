c252a5b2f5f6541ade7aa8320d01cff95020e7e6
Ilya Vorontsov <vorontsov.i.e@gmail.com>
2021-04-15 13:31:02 +0300

    fix folder names

--- postprocessing/copy_motifs.sh
+++ postprocessing/copy_motifs.sh
@@ -5,7 +5,9 @@ MOTIFS_DESTINATION=/home_local/vorontsovie/greco-bit-data-processing/release_6_m
 mkdir -p ${MOTIFS_DESTINATION}/VIGG/
 cp -Tr /home_local/pavelkrav/GRECO_2_iter_pcms/CHS  ${MOTIFS_DESTINATION}/VIGG/CHS
 cp -Tr /home_local/pavelkrav/GRECO_2_iter_pcms/AFS  ${MOTIFS_DESTINATION}/VIGG/AFS.Peaks
-cp -r /home_local/arsen_l/greco-bit/motifs/motif_collection_release_6.2021-02-13/{HTS,SMS,AFS.Reads}/pcms  ${MOTIFS_DESTINATION}/VIGG/
+cp -Tr /home_local/arsen_l/greco-bit/motifs/motif_collection_release_6.2021-02-13/HTS/pcms  ${MOTIFS_DESTINATION}/VIGG/HTS
+cp -Tr /home_local/arsen_l/greco-bit/motifs/motif_collection_release_6.2021-02-13/SMS/pcms  ${MOTIFS_DESTINATION}/VIGG/SMS
+cp -Tr /home_local/arsen_l/greco-bit/motifs/motif_collection_release_6.2021-02-13/AFS.Reads/pcms  ${MOTIFS_DESTINATION}/VIGG/AFS.Reads
 cp -Tr /home_local/vorontsovie/greco-bit-data-processing/process_PBM/release_6_motifs/pcms  ${MOTIFS_DESTINATION}/VIGG/PBM
 
 mkdir -p ${MOTIFS_DESTINATION}/jangrau/
