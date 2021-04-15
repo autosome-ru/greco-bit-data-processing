0e7b58e155d76dfea67ed0f0eae3583bdb246352
Ilya Vorontsov <vorontsov.i.e@gmail.com>
2021-04-15 15:56:04 +0300

    motif validator doesn't look for header

--- postprocessing/motif_validator.rb
+++ postprocessing/motif_validator.rb
@@ -1,4 +1,4 @@
-BN_PATTERN = /^(?<tf>[^.@]+\.[^.@]+)@(?<exp_type>HTS.IVT|HTS.Lys|AFS.IVT|AFS.Lys|PBM|SMS|CHS)@(?<dataset>[-+a-z]+)@(?<tool>[^@.]+\.[^@.]+)@(?<motif_subname>[^@.]+)$/
+BN_PATTERN = /^(?<tf>[^.@]+\.[^.@]+)@(?<exp_type>HTS.IVT|HTS.Lys|AFS.IVT|AFS.Lys|PBM.ME|SMS|CHS)@(?<dataset>[-+a-z]+)@(?<tool>[^@.]+\.[^@.]+)@(?<motif_subname>[^@.]+)$/
 PPM_TOLERANCE = 0.05
 filelist = ARGV
 filelist.select{|fn|
@@ -12,7 +12,8 @@ filelist.select{|fn|
   header = content.first
   report << "Filename has incorrect extension `#{ext}`"  unless ['.pcm', '.ppm', '.pwm'].include?(ext)
   report << "Filename doesn't match pattern tf.construct@{PBM,CHS,SMS,HTS.IVT,HTS.Lys,AFS.Lys,AFS.IVT}@dataset_id@team.tool@motif_name_wo_dots"  unless bn_wo_ext.match(BN_PATTERN)
-  report << "Header `#{header}` doesn't follow pattern >motif_name[ optional infos]"  unless (header == ">#{bn_wo_ext}") || header.start_with?(">#{bn_wo_ext} ") || (header == "> #{bn_wo_ext}") || header.start_with?("> #{bn_wo_ext} ")
+  report << "No header"  unless header.start_with?('>')
+  # report << "Header `#{header}` doesn't follow pattern >motif_name[ optional infos]"  unless (header == ">#{bn_wo_ext}") || header.start_with?(">#{bn_wo_ext} ") || (header == "> #{bn_wo_ext}") || header.start_with?("> #{bn_wo_ext} ")
   begin
     matrix = content.drop(1).map{|l| l.split }.map{|r| r.map{|x| Float(x) } }
     report << "Each line should have 4-columns"  unless matrix.all?{|r| r.size == 4 }
