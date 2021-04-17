e75323be81cd5448a9eb942ae06aa8d963f087b5
Ilya Vorontsov <vorontsov.i.e@gmail.com>
2021-04-17 10:16:05 +0300

    fix

--- postprocessing/motif_metrics_reads.rb
+++ postprocessing/motif_metrics_reads.rb
@@ -58,6 +58,7 @@ tfs.each{|tf|
     flank_3 = grp_rest.select{|f| f.match? /^3[ACGT]+$/ }.take_the_only[1..-1]
 
     dataset_infos = datasets.map{|ds|
+      bn = File.basename(dataset)
       _tf_info, _exp_type, exp_info, ds_info = bn.split('@')
       _exp_id, *rest = exp_info.split('.')
       cycle = rest.select{|f| f.match? /^C\d$/ }.take_the_only
