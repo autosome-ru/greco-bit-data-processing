849a11ade27a38a7d0df85451cba1df853238408
Ilya Vorontsov <vorontsov.i.e@gmail.com>
2021-05-02 10:36:03 +0300

    small refactoring (rename results folder -> intermediate folder)

--- process_data_CHS.sh
+++ process_data_CHS.sh
@@ -1,62 +1,63 @@
 SOURCE_FOLDER=./source_data/CHS/
-RESULTS_FOLDER=./results_databox_chs/
+INTERMEDIATE_FOLDER=./results_databox_chs/
+RESULTS_FOLDER=./source_data_prepared/CHS
 SCRIPT_FOLDER=./process_peaks_CHS_AFS/
 
-mkdir -p "${RESULTS_FOLDER}"
+mkdir -p "${INTERMEDIATE_FOLDER}"
 
-ruby "${SCRIPT_FOLDER}/prepare_peaks_chipseq.rb" "${SOURCE_FOLDER}" "${RESULTS_FOLDER}"
+ruby "${SCRIPT_FOLDER}/prepare_peaks_chipseq.rb" "${SOURCE_FOLDER}" "${INTERMEDIATE_FOLDER}"
 
-mkdir -p ${RESULTS_FOLDER}/Train_sequences/
-for FN in $(find ${RESULTS_FOLDER}/Train_intervals/ -xtype f); do
+mkdir -p ${INTERMEDIATE_FOLDER}/Train_sequences/
+for FN in $(find ${INTERMEDIATE_FOLDER}/Train_intervals/ -xtype f); do
   TF="$(basename -s .interval "$FN")"
-  cat ${FN} | tail -n+2 | sort -k5,5nr | head -500 | ./bedtools getfasta -fi ./source_data/hg38.fa -bed - > ${RESULTS_FOLDER}/Train_sequences/${TF}.fa
+  cat ${FN} | tail -n+2 | sort -k5,5nr | head -500 | ./bedtools getfasta -fi ./source_data/hg38.fa -bed - > ${INTERMEDIATE_FOLDER}/Train_sequences/${TF}.fa
 done
 
-mkdir -p ${RESULTS_FOLDER}/Val_sequences/
-for FN in $(find ${RESULTS_FOLDER}/Val_intervals/ -xtype f); do
+mkdir -p ${INTERMEDIATE_FOLDER}/Val_sequences/
+for FN in $(find ${INTERMEDIATE_FOLDER}/Val_intervals/ -xtype f); do
   TF="$(basename -s .interval "$FN")"
-  cat ${FN} | tail -n+2 | sort -k5,5nr | head -500 | ./bedtools getfasta -fi ./source_data/hg38.fa -bed - > ${RESULTS_FOLDER}/Val_sequences/${TF}.fa
+  cat ${FN} | tail -n+2 | sort -k5,5nr | head -500 | ./bedtools getfasta -fi ./source_data/hg38.fa -bed - > ${INTERMEDIATE_FOLDER}/Val_sequences/${TF}.fa
 done
 
 
-mkdir -p source_data_prepared/CHS/Train_intervals source_data_prepared/CHS/Val_intervals 
-mkdir -p source_data_prepared/CHS/Train_sequences source_data_prepared/CHS/Val_sequences 
+mkdir -p ${RESULTS_FOLDER}/Train_intervals ${RESULTS_FOLDER}/Val_intervals
+mkdir -p ${RESULTS_FOLDER}/Train_sequences ${RESULTS_FOLDER}/Val_sequences
 
-for FN in $(find ${RESULTS_FOLDER}/Train_intervals/ -xtype f ); do
+for FN in $(find ${INTERMEDIATE_FOLDER}/Train_intervals/ -xtype f ); do
     # BN=$(basename -s .pbm.txt ${FN})
     NEW_BN=$( ruby ${SCRIPT_FOLDER}/name_sample_chs.rb "$FN" --extension peaks )
     if [[ -n "$NEW_BN" ]]; then
-        cp ${FN} source_data_prepared/CHS/Train_intervals/${NEW_BN}
+        cp ${FN} ${RESULTS_FOLDER}/Train_intervals/${NEW_BN}
     else
         echo "Can't get filename for ${FN}. Probably no metadata supplied" >& 2
     fi
 done
 
-for FN in $(find ${RESULTS_FOLDER}/Val_intervals/ -xtype f ); do
+for FN in $(find ${INTERMEDIATE_FOLDER}/Val_intervals/ -xtype f ); do
     # BN=$(basename -s .pbm.txt ${FN})
     NEW_BN=$( ruby ${SCRIPT_FOLDER}/name_sample_chs.rb "$FN" --extension peaks )
     if [[ -n "$NEW_BN" ]]; then
-        cp ${FN} source_data_prepared/CHS/Val_intervals/${NEW_BN}
+        cp ${FN} ${RESULTS_FOLDER}/Val_intervals/${NEW_BN}
     else
         echo "Can't get filename for ${FN}. Probably no metadata supplied" >& 2
     fi
 done
 
-for FN in $(find ${RESULTS_FOLDER}/Train_sequences/ -xtype f ); do
+for FN in $(find ${INTERMEDIATE_FOLDER}/Train_sequences/ -xtype f ); do
     # BN=$(basename -s .pbm.txt ${FN})
     NEW_BN=$( ruby ${SCRIPT_FOLDER}/name_sample_chs.rb "$FN" --extension fa )
     if [[ -n "$NEW_BN" ]]; then
-        cp ${FN} source_data_prepared/CHS/Train_sequences/${NEW_BN}
+        cp ${FN} ${RESULTS_FOLDER}/Train_sequences/${NEW_BN}
     else
         echo "Can't get filename for ${FN}. Probably no metadata supplied" >& 2
     fi
 done
 
-for FN in $(find ${RESULTS_FOLDER}/Val_sequences/ -xtype f ); do
+for FN in $(find ${INTERMEDIATE_FOLDER}/Val_sequences/ -xtype f ); do
     # BN=$(basename -s .pbm.txt ${FN})
     NEW_BN=$( ruby ${SCRIPT_FOLDER}/name_sample_chs.rb "$FN" --extension fa )
     if [[ -n "$NEW_BN" ]]; then
-        cp ${FN} source_data_prepared/CHS/Val_sequences/${NEW_BN}
+        cp ${FN} ${RESULTS_FOLDER}/Val_sequences/${NEW_BN}
     else
         echo "Can't get filename for ${FN}. Probably no metadata supplied" >& 2
     fi
