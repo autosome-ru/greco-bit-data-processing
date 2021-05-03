3e3732f5b74416ea17ab23624cd58e928cc5cafe
Ilya Vorontsov <vorontsov.i.e@gmail.com>
2021-05-03 13:48:55 +0300

    refactor

---
+++ process_peaks_CHS_AFS/naming_chs.rb
@@ -0,0 +1,91 @@
+require 'optparse'
+require_relative '../shared/lib/index_by'
+require_relative '../shared/lib/plasmid_metadata'
+require_relative '../shared/lib/random_names'
+require_relative 'chipseq_metadata'
+
+module Chipseq
+  def self.gen_name(sample_metadata, slice_type:, extension:)
+    experiment_id = sample_metadata.experiment_id
+    tf = sample_metadata.gene_id
+    construct_type = sample_metadata.construct_type
+    basename = "#{tf}.#{construct_type}@CHS@#{experiment_id}@Peaks"
+
+    uuid = take_dataset_name!
+    "#{basename}.#{uuid}.#{slice_type}.#{extension}"
+  end
+
+  def self.sample_basename(sample_metadata)
+    experiment_id = sample_metadata.experiment_id
+    tf = sample_metadata.gene_id
+    construct_type = sample_metadata.construct_type
+    basename = "#{tf}.#{construct_type}@CHS@#{experiment_id}@Peaks"
+  end
+
+  def self.generate_name(sample_metadata, slice_type:, extension:)
+    basename = sample_basename(sample_metadata)
+
+    uuid = take_dataset_name!
+    "#{basename}.#{uuid}.#{slice_type}.#{extension}"
+  end
+
+  def self.find_names(folder, sample_metadata, slice_type:, extension:)
+    basename = sample_basename(sample_metadata)
+    Dir.glob(File.join(folder, "#{basename}.*.#{slice_type}.#{extension}"))
+  end
+
+  def self.find_name(folder, sample_metadata, slice_type:, extension:)
+    fns = find_names(folder, sample_metadata, slice_type: slice_type, extension: extension)
+    if fns.size == 1
+      fns.first
+    else
+      $stderr.puts "Can't choice a candidate from multiple variants: #{fns.join(', ')}"  if fns.size > 1
+      nil
+    end
+  end
+
+  def self.determine_slice_type(sample_fn)
+    orig_slice_type = File.basename(sample_fn).split('.')[4]
+    case orig_slice_type
+    when 'train'
+      'Train'
+    when 'basic_val'
+      'Val-B'
+    when /^advanced_val/
+      'Val-A'
+    else
+      raise "Unknown slice type `#{orig_slice_type}` for `#{sample_fn}`"
+    end
+  end
+
+  def self.main
+    slice_type = nil
+    extension = nil
+    argparser = OptionParser.new{|o|
+      o.on('--slice-type VAL', 'Train or Val') {|v| slice_type = v }
+      o.on('--extension VAL', 'fa or peaks') {|v| extension = v }
+    }
+
+    argparser.parse!(ARGV)
+    sample_fn = ARGV[0]
+    raise 'Specify slice type (Train / Val-B / Val-A)'  unless [nil, 'Train', 'Val-B', 'Val-A'].include?(slice_type)
+    raise 'Specify extension (fa or peaks)'  unless ['fa', 'peaks'].include?(extension)
+    raise 'Specify sample filename'  unless sample_fn
+    raise 'Sample file not exists'  unless File.exist?(sample_fn)
+
+    plasmids_metadata = PlasmidMetadata.each_in_file('source_data_meta/shared/Plasmids.tsv').to_a
+    $plasmid_by_number = plasmids_metadata.index_by(&:plasmid_number)
+
+    metadata = Chipseq::SampleMetadata.each_in_file('source_data_meta/CHS/CHS.tsv').to_a
+
+    slice_type ||= determine_slice_type(sample_fn)
+    # assay_id = File.basename(sample_fn).split('_')[0]
+    data_file_id = File.basename(sample_fn).split('.')[1]
+    normalized_id = data_file_id.sub(/_L\d+(\+L\d+)?$/, "").sub(/_\d_pf(\+\d_pf)?$/,"").sub(/_[ACGT]{6}$/, "")
+    sample_metadata = metadata.detect{|m| m.normalized_id == normalized_id }
+
+    if sample_metadata
+      puts self.generate_name(sample_metadata, slice_type: slice_type, extension: extension)
+    end
+  end
+end
