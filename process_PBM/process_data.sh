ee26c3e7312273937d8a59514b18a60337e9242b
Ilya Vorontsov <vorontsov.i.e@gmail.com>
2021-07-05 17:33:43 +0300

    PBM Train/Val are now generated for both chip types

--- process_PBM/process_data.sh
+++ process_PBM/process_data.sh
@@ -94,32 +94,22 @@ ruby ${SCRIPT_FOLDER}/zscore_transform_chips.rb \
 for PROCESSING_TYPE in SDQN QNZS; do
     mkdir -p source_data_prepared/PBM.${PROCESSING_TYPE}/Train_intensities  source_data_prepared/PBM.${PROCESSING_TYPE}/Val_intensities
 
-    for FN in $(find ${INTERMEDIATE_FOLDER}/${PROCESSING_TYPE}_intensities/ -xtype f -name '*_1M-ME_*'); do
-        NEW_DIRNAME="source_data_prepared/PBM.${PROCESSING_TYPE}/Train_intensities"
-        NEW_BN=$( ruby ${SCRIPT_FOLDER}/name_sample_pbm.rb "$FN" --slice-type Train --extension tsv --processing-type ${PROCESSING_TYPE} --action find-or-generate --lookup-folder ${NEW_DIRNAME} )
-        if [[ -n "$NEW_BN" ]]; then
-            if [[ -s "${NEW_DIRNAME}/${NEW_BN}" ]]; then # file exists and has a size greater than zero.
-                echo "${NEW_DIRNAME}/${NEW_BN} already exists" >&2
-            else
-                cp "${FN}" "${NEW_DIRNAME}/${NEW_BN}"
-            fi
-        else
-            echo "Can't get filename for ${FN}. Probably no metadata supplied" >&2
-        fi
-    done
-
-    for FN in $(find ${INTERMEDIATE_FOLDER}/${PROCESSING_TYPE}_intensities/ -xtype f -name '*_1M-HK_*'); do
-        NEW_DIRNAME="source_data_prepared/PBM.${PROCESSING_TYPE}/Val_intensities"
-        NEW_BN=$( ruby ${SCRIPT_FOLDER}/name_sample_pbm.rb "$FN" --slice-type Val --extension tsv --processing-type ${PROCESSING_TYPE} --action find-or-generate --lookup-folder ${NEW_DIRNAME} )
-        if [[ -n "$NEW_BN" ]]; then
-            if [[ -s "${NEW_DIRNAME}/${NEW_BN}" ]]; then # file exists and has a size greater than zero.
-                echo "${NEW_DIRNAME}/${NEW_BN} already exists" >&2
-            else
-                cp "${FN}" "${NEW_DIRNAME}/${NEW_BN}"
-            fi
-        else
-            echo "Can't get filename for ${FN}. Probably no metadata supplied" >&2
-        fi
+    for SLICE_TYPE in Train Val; do
+        for CHIP_TYPE in ME HK; do
+            for FN in $(find ${INTERMEDIATE_FOLDER}/${PROCESSING_TYPE}_intensities/ -xtype f -name "*_1M-${CHIP_TYPE}_*"); do
+                NEW_DIRNAME="source_data_prepared/PBM.${PROCESSING_TYPE}/${SLICE_TYPE}_intensities"
+                NEW_BN=$( ruby ${SCRIPT_FOLDER}/name_sample_pbm.rb "$FN" --slice-type ${SLICE_TYPE} --extension tsv --processing-type ${PROCESSING_TYPE} --action find-or-generate --lookup-folder ${NEW_DIRNAME} )
+                if [[ -n "$NEW_BN" ]]; then
+                    if [[ -s "${NEW_DIRNAME}/${NEW_BN}" ]]; then # file exists and has a size greater than zero.
+                        echo "${NEW_DIRNAME}/${NEW_BN} already exists" >&2
+                    else
+                        cp "${FN}" "${NEW_DIRNAME}/${NEW_BN}"
+                    fi
+                else
+                    echo "Can't get filename for ${FN}. Probably no metadata supplied" >&2
+                fi
+            done
+        done
     done
 
     for SLICE_TYPE in Train Val; do
