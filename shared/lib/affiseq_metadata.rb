8d1d0b15303d1c4b3e2926c2272cb3dedb16e9ce
Ilya Vorontsov <vorontsov.i.e@gmail.com>
2021-08-06 12:35:05 +0300

    fix bugs with reading AFS metadata (some filenames were not considered)

--- shared/lib/affiseq_metadata.rb
+++ shared/lib/affiseq_metadata.rb
@@ -8,13 +8,18 @@ module Affiseq
 
     def construct_type; $plasmid_by_number[plasmid_id].construct_type; end
     def filenames
-      [cycle_1_filename, cycle_2_filename, cycle_3_filename].compact
+      [
+        cycle_1_filename, cycle_2_filename, cycle_3_filename, cycle_4_filename,
+        cycle_1_read_2_filename, cycle_2_read_2_filename, cycle_3_read_2_filename,
+      ].compact
     end
 
     def self.from_string(line)
       # Example:
       ## Experiment ID Plasmid ID  Gene name IVT or Lysate DNA library ID  Well  Filename Read1 Cycle1 Filename Read1 Cycle2 Filename Read1 Cycle3
       ## AATA_AffSeq_D5_GLI4 pTH15820  GLI4  Lysate  AffiSeqV1 D5  GLI4_AffSeq_Lysate_BatchAATA_Cycle1_R1.fastq.gz GLI4_AffSeq_Lysate_BatchAATA_Cycle2_R1.fastq.gz GLI4_AffSeq_Lysate_BatchAATA_Cycle3_R1.fastq.gz
+      fn_converter = ->(fn){ (fn.start_with?('No cycle ') || fn.start_with?('No read ')) ? nil : fn }
+
       experiment_id, plasmid_id, gene_name, ivt_or_lysate, dna_library_id, well, \
         cycle_1_filename, cycle_2_filename, cycle_3_filename, cycle_4_filename, \
         cycle_1_read_2_filename, cycle_2_read_2_filename, cycle_3_read_2_filename, \
@@ -23,8 +28,11 @@ module Affiseq
       self.new(
         experiment_id: experiment_id, plasmid_id: plasmid_id, gene_name: gene_name,
         ivt_or_lysate: ivt_or_lysate[0,3], dna_library_id: dna_library_id, well: well,
-        cycle_1_filename: cycle_1_filename, cycle_2_filename: cycle_2_filename, cycle_3_filename: cycle_3_filename, cycle_4_filename: cycle_4_filename,
-        cycle_1_read_2_filename: cycle_1_read_2_filename, cycle_2_read_2_filename: cycle_2_read_2_filename, cycle_3_read_2_filename: cycle_3_read_2_filename,
+        cycle_1_filename: fn_converter.call(cycle_1_filename), cycle_2_filename: fn_converter.call(cycle_2_filename),
+        cycle_3_filename: fn_converter.call(cycle_3_filename), cycle_4_filename: fn_converter.call(cycle_4_filename),
+        cycle_1_read_2_filename: fn_converter.call(cycle_1_read_2_filename),
+        cycle_2_read_2_filename: fn_converter.call(cycle_2_read_2_filename),
+        cycle_3_read_2_filename: fn_converter.call(cycle_3_read_2_filename),
         folder: folder,
       )
     end
