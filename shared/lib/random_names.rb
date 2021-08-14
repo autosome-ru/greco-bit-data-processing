aa0488df12a5224f6e2bf5a27bdcde5bb45a6e35
Ilya Vorontsov <vorontsov.i.e@gmail.com>
2021-08-14 16:33:10 +0300

    raise if dataset name pool exhausted

--- shared/lib/random_names.rb
+++ shared/lib/random_names.rb
@@ -4,15 +4,21 @@ require "sqlite3"
 $dataset_names_DB = SQLite3::Database.new('dataset_names.db')
 def take_dataset_name!
   db = $dataset_names_DB
-  while true
-    begin
-      db.transaction(:exclusive) {
-        ds_id, ds_name = db.execute("SELECT id, name FROM dataset_names WHERE status == 'not-used' LIMIT 1").first
+  ds_name = nil
+  max_num_attempts = 10
+  num_attempts = max_num_attempts
+  while ds_name.nil? && num_attempts > 0
+    db.transaction(:exclusive) {
+      ds_id, ds_name = db.execute("SELECT id, name FROM dataset_names WHERE status == 'not-used' LIMIT 1").first
+      if ds_name
         db.execute("UPDATE dataset_names SET status = 'used' WHERE id == (?)", [ds_id])
         return ds_name
-      }
-    rescue => e
-      sleep(rand * 5) # wait and retry
-    end
+      end
+    }
+    num_attempts -= 1
+    sleep(rand * 5) # wait and retry
   end
+  count_used, = db.execute("SELECT COUNT(*) FROM dataset_names WHERE status == 'used'").first
+  count_free, = db.execute("SELECT COUNT(*) FROM dataset_names WHERE status == 'not-used'").first
+  raise "take_dataset_name!: #{max_num_attempts} attempts failed. There are #{count_free} unused names in the pool, while #{count_used} names used."
 end
