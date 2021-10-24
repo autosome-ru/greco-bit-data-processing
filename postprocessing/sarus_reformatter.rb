b6b53db2ea1ed4e1de52d271ff04ee3537ea73db
Ilya Vorontsov <vorontsov.i.e@gmail.com>
2021-10-24 11:59:59 +0300

    SARUS reformatter now can filter by TF or not (for AFS where flanks doesn't depend on TF)

--- postprocessing/sarus_reformatter.rb
+++ postprocessing/sarus_reformatter.rb
@@ -1,14 +1,21 @@
+filter_by_tf = ARGV.delete('--filter-by-tf')
 raise 'Specify motif name'  unless motif = ARGV[0]
 
 motif_tf = motif.split(".")[0]
 
-$stdin.each_line.each_slice(2).map{|hdr,scores|
+infos = $stdin.each_line.each_slice(2).map{|hdr,scores|
   tf, exp_id, type = hdr.chomp[1..-1].split(":")
   logpval, pos, strand = scores.chomp.split("\t")
   [tf, exp_id, type, logpval, pos, strand]
-}.select{|tf, *_rest|
-  motif_tf == tf
-}.each{|info|
+}
+
+if filter_by_tf
+  infos = infos.select{|tf, *_rest|
+    motif_tf == tf
+  }
+end
+
+infos.each{|info|
   info = [motif, *info]
   puts info.join("\t")
 }
