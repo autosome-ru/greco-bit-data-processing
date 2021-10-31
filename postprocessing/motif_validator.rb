3f8eb1054742ea21e5fc30a7ac46fe8fae47c74e
Ilya Vorontsov <vorontsov.i.e@gmail.com>
2021-10-31 17:46:25 +0300

    fix validator

--- postprocessing/motif_validator.rb
+++ postprocessing/motif_validator.rb
@@ -16,7 +16,7 @@ filelist.select{|fn|
   # report << "Header `#{header}` doesn't follow pattern >motif_name[ optional infos]"  unless (header == ">#{bn_wo_ext}") || header.start_with?(">#{bn_wo_ext} ") || (header == "> #{bn_wo_ext}") || header.start_with?("> #{bn_wo_ext} ")
   begin
     matrix_rows = content.drop(1).map{|l| l.split("\t") }
-    report << "Format motifs in non-scientific format"  unless matrix_rows.all?{|r| r.match?(/^[-+]?\d+(\.\d+)$/) }
+    report << "Format motifs in non-scientific format"  unless matrix_rows.all?{|row| row.all?{|el| el.match?(/^[-+]?\d+(\.\d+)?$/) } }
     matrix = matrix_rows.map{|r| r.map{|x| Float(x) } }
     report << "Each line should have 4-columns"  unless matrix.all?{|r| r.size == 4 }
     report << "Not all lines of probabiities matrix sum to 1.0"  if ext == '.ppm' && !matrix.all?{|r| (r.sum - 1.0).abs < PPM_TOLERANCE }
