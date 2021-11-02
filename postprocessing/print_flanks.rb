83e7c6dc8af0c785d6ba313ee5e1e5442eb869d9
Ilya Vorontsov <vorontsov.i.e@gmail.com>
2021-11-02 23:39:04 +0300

    added printing flanks for SMS

--- postprocessing/print_flanks.rb
+++ postprocessing/print_flanks.rb
@@ -1,5 +1,7 @@
 require_relative '../process_reads_HTS_SMS_AFS/hts'
 require_relative '../shared/bin/name_sample_afs'
+require_relative '../process_reads_HTS_SMS_AFS/sms_unpublished'
+require_relative '../process_reads_HTS_SMS_AFS/sms_published'
 
 hts_metadata = Selex::SampleMetadata.each_in_file('source_data_meta/HTS/HTS.tsv').to_a
 # afs_metadata = Affiseq::SampleMetadata.each_in_file('source_data_meta/AFS/AFS.tsv').to_a
@@ -24,4 +26,18 @@ File.open('AFS_flanks.fa', 'w'){|fw|
   fw.puts(AffiseqPeaks::ADAPTER_5)
   fw.puts('> all:all:3-prime')
   fw.puts(AffiseqPeaks::ADAPTER_3)
+}
+
+File.open('SMS_unpublished_flanks.fa', 'w'){|fw|
+  fw.puts('> all:all:5-prime')
+  fw.puts(SMSUnpublished::ADAPTER_5)
+  fw.puts('> all:all:3-prime')
+  fw.puts(SMSUnpublished::ADAPTER_3)
+}
+
+File.open('SMS_published_flanks.fa', 'w'){|fw|
+  fw.puts('> all:all:5-prime')
+  fw.puts(SMSPublished::ADAPTER_5)
+  fw.puts('> all:all:3-prime')
+  fw.puts(SMSPublished::ADAPTER_3)
 }
