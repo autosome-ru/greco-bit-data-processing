35c5264f5847012420e9144854c0f0115b2e4938
Ilya Vorontsov <vorontsov.i.e@gmail.com>
2021-11-09 12:25:52 +0300

    join PBM.HK, PBM.ME in rankings

--- postprocessing/motif_ranking.rb
+++ postprocessing/motif_ranking.rb
@@ -446,10 +446,12 @@ all_metric_infos.select!{|info|
 
 all_metric_infos = all_metric_infos.map{|info|
   dataset = info[:dataset]
+  exp_type = experiment_fulltype(dataset)
+  exp_type = 'PBM'  if ['PBM.ME' || 'PBM.HK'].include?(exp_type) # distinct chip types are not too different to distinguish them
   additional_info = {
     processing_type: experiment_processing_type(dataset),
     experiment: experiment_id(dataset),
-    experiment_type: experiment_fulltype(dataset),
+    experiment_type: exp_type,
   }
   info.merge(additional_info)
 }
