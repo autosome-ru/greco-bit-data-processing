d472b57fc982b99ece25d9b6602e9b642ab869ed
Ilya Vorontsov <vorontsov.i.e@gmail.com>
2021-12-01 13:26:54 +0300

    corrected TF-DBD mapping

--- postprocessing/collect_dbds.rb
+++ postprocessing/collect_dbds.rb
@@ -16,6 +16,8 @@ def normalize(val)
   end    
 end
 
+domain_mapping = {'ETS' => 'Ets', 'Homeobox' => 'Homeodomain', 'HMG/Sox' => 'Sox'}
+
 cisbp_dbd_info = CSV.foreach('source_data_meta/shared/cisbp_TF_Information_all_motifs_plus.txt', col_sep: "\t", headers: true).each_with_object({}){|row, hsh|
   tf, family, dbds, dbd_count = row.values_at('TF_Name', 'Family_Name', 'DBDs', 'DBD_Count')
   hsh[tf] = {family: family, dbds: dbds, dbd_count: dbd_count && Integer(dbd_count)}
@@ -34,8 +36,14 @@ tf_infos = File.readlines(metadata_fn).map{|l|
   hsh[tf]["dbd"] += dbd
   hsh[tf]["dbd_human"] += dbd_human
 }.map{|tf, info|
-  cisbp_fams = (cisbp_dbd_info.dig(tf, :family) || '').split(',').reject{|v| v == 'Unknown' }
-  fams = (info['dbd'] + info['dbd_human'] + cisbp_fams).uniq.sort
+  fams = (info['dbd'] + info['dbd_human']).uniq.sort
+  if fams.empty?
+    fams = (cisbp_dbd_info.dig(tf, :family) || '').split(',').reject{|v| v == 'Unknown' }
+  end
+  
+  fams = fams.map{|fam| domain_mapping.fetch(fam, fam) }.uniq
+  fams.delete('HMG')  if fams.include?('Sox') # Sox family is inside HMG
+
   [tf, fams]
 }.to_h
 
