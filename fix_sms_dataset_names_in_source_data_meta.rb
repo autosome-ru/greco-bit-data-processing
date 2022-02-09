cc1fafa2b154579f7def11876c21a95b03e83528
Ilya Vorontsov <vorontsov.i.e@gmail.com>
2022-02-10 00:32:08 +0300

    update SMS ids

---
+++ fix_sms_dataset_names_in_source_data_meta.rb
@@ -0,0 +1,16 @@
+counter = {}
+File.open('source_data_meta/SMS/unpublished/SMS_fix_2022-02-09.tsv', 'w'){|fw|
+  File.readlines('source_data_meta/SMS/unpublished/SMS.tsv').map(&:chomp).map{|l|
+    l.split("\t")
+  }.each{|row|
+    exp_id = row[0].split('-').first(2).join('-')
+    if counter.has_key?(exp_id)
+      counter[exp_id] += 1
+      corrected_exp_id = "#{exp_id}-#{counter[exp_id]}"
+    else
+      counter[exp_id] = 1
+      corrected_exp_id = row[0]
+    end
+    fw.puts([corrected_exp_id, *row.drop(1)].join("\t"))
+  }
+}
