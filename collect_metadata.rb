030a649d208939ab4a95edfe2a292d17175473f2
Ilya Vorontsov <vorontsov.i.e@gmail.com>
2022-08-01 17:33:37 +0000

    update collect_metadata

--- collect_metadata.rb
+++ collect_metadata.rb
@@ -18,7 +18,7 @@ require_relative 'process_peaks_CHS_AFS/chipseq_metadata'
 require_relative 'process_peaks_CHS_AFS/experiment_info_chs'
 require_relative 'process_peaks_CHS_AFS/experiment_info_afs'
 
-RELEASE_FOLDER = '/home_local/vorontsovie/greco-data/release_8c.2022-06-01/full'
+RELEASE_FOLDER = '/home_local/vorontsovie/greco-data/release_8d.2022-07-31/full'
 SOURCE_FOLDER = '/home_local/vorontsovie/greco-bit-data-processing/source_data'
 MYSQL_CONFIG = {host: 'localhost', username: 'vorontsovie', password: 'password'}
 
