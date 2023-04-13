d646295d68ad15718ba7324a7b1d7cdddf08b7f0
Ilya Vorontsov <vorontsov.i.e@gmail.com>
2023-04-13 16:45:35 +0300

    fix

--- postprocessing/motif_metrics_pbm.rb
+++ postprocessing/motif_metrics_pbm.rb
@@ -69,10 +69,10 @@ tfs.each{|tf|
             "--security-opt apparmor=unconfined",
             "--name #{container_name}",
             "--volume #{MOTIFS_PATH}:/motifs/:ro",
-            "--volume #{dataset_txt}:/pbm_data.txt:ro",
+            "--volume #{dataset_txt}:/pbm_data/#{dataset_bn}:ro",
             "--env JAVA_OPTIONS=-Xmx2G",
             "vorontsovie/pwmbench_pbm:1.3.2",
-            "ROC,PR", "/pbm_data.txt", "-",
+            "ROC,PR", "/pbm_data/#{dataset_bn}", "-",
       ].join(" ")
       fw.puts("#{cmd} || echo")
     end
