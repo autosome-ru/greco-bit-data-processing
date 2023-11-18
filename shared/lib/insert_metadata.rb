ad10b0e17ce6d387b4059232e98d3b8189c9ad39
Ilya Vorontsov <vorontsov.i.e@gmail.com>
2023-11-18 21:47:06 +0300

    fix plasmid_numbers for insert (some plasmid numbers were missing and thus inserts were not matched)

--- shared/lib/insert_metadata.rb
+++ shared/lib/insert_metadata.rb
@@ -14,11 +14,11 @@ InsertMetadata = Struct.new(*[
   def self.from_string(line)
     insert_id, source_tf_gene, dbd_type, dbd_type_from_HumanTFs, 
       amino_acid_sequence, aa_length, recoded_dna_sequence, dna_length, 
-      smileseq_eGFP_clone_id, smileseq_biobasic_UT380_clone, smileseq_missing_name, smileseq_plasmid_backbone,
+      smileseq_eGFP_clone_id, biobasic_UT380_clone, smileseq_missing_name, smileseq_plasmid_backbone,
       cell_culture_eGFP_id, eGFP_biobasic_UT368_no_long, eGFP_biobasic_UT368_no_short, eGFP_plasmid_number, eGFP_plasmid_name, eGFP_plasmid_backbone,
       t7_GST_clone_id, t7_GST_biobasic_UT368_no_long, t7_GST_biobasic_UT368_no_short, t7_GST_plasmid_number, t7_GST_plasmid_name, t7_GST_plasmid_backbone,
       ally_comparasion_list, insert_description, = line.chomp.split("\t", 26)
-      plasmid_numbers = [parse_cell(eGFP_plasmid_number), parse_cell(t7_GST_plasmid_number)].compact
+      plasmid_numbers = [parse_cell(eGFP_plasmid_number), parse_cell(t7_GST_plasmid_number), parse_cell(biobasic_UT380_clone)].compact
     self.new(
       insert_id: insert_id, source_tf_gene: source_tf_gene, dbd_type: parse_cell(dbd_type), dbd_type_from_HumanTFs: parse_cell_list(dbd_type_from_HumanTFs),
       amino_acid_sequence: amino_acid_sequence.strip, aa_length: parse_int(aa_length), recoded_dna_sequence: recoded_dna_sequence.strip, dna_length: parse_int(dna_length),
