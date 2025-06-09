9783a8cc70970d15683d87750dfbcbb994ba17e9
Ilya Vorontsov <vorontsov.i.e@gmail.com>
2025-06-10 02:50:16 +0300

    rename motifs in similaities to hocomoco

--- postprocessing/fix_tf_names_codebook_bug-stage-3.rb
+++ postprocessing/fix_tf_names_codebook_bug-stage-3.rb
@@ -4,20 +4,25 @@ require 'fileutils'
 require 'set'
 require_relative 'fix_tf_names_codebook_bug_utils.rb'
 
-def motif_similarity_rename_pairs(datasets, renames)
-  dataset_ids_renames = get_dataset_ids_renames(datasets, renames)
+def renamed_motif_basename(fn, dataset_ids_renames)
+  renames_for_motif = dataset_ids_by_motif_fn(fn).map{|dataset_id|
+    dataset_ids_renames[dataset_id]
+  }.compact.uniq
+
+  if renames_for_motif.size == 0
+    File.basename(fn)
+  elsif renames_for_motif.size == 1
+    renamed_motif_basename_by_info(fn, renames_for_motif.take_the_only)
+  else
+    raise "Mismatch in motif renames"
+  end
+end
+
+def motif_similarity_rename_pairs(dataset_ids_renames)
   Dir.glob("artifact_sims_precise/*").map{|fn|
-    renames_for_motif = dataset_ids_by_motif_fn(fn).map{|dataset_id| dataset_ids_renames[dataset_id] }.compact.uniq
-    if renames_for_motif.size == 0
-      [fn, File.join('artifact_sims_precise_recalc', File.basename(fn))]
-    elsif renames_for_motif.size == 1
-      rename_info = renames_for_motif.take_the_only
-      new_basename = renamed_motif_basename_by_info(fn, rename_info)
-      [fn, File.join('artifact_sims_precise_recalc', new_basename)]
-    else
-      raise "Mismatch in motif renames"
-    end
-  }.compact
+    new_bn = renamed_motif_basename(fn, dataset_ids_renames)
+    [fn, File.join('artifact_sims_precise_recalc', new_bn)]
+  }
 end
 
 FileUtils.rm_rf('artifact_sims_precise_recalc')
@@ -31,5 +36,16 @@ renames = CSV.foreach('source_data_meta/fixes/CODEGATE_DatasetsSwap.txt', col_se
 
 datasets_full = File.readlines('freeze/datasets_metadata.full.json').map{|l| JSON.parse(l) }
 
+dataset_ids_renames = get_dataset_ids_renames(datasets, renames)
 # copy/copy-and-rename motif-related files from `artifact_sims_precise` to `artifact_sims_precise_recalc`
-copy_files(motif_similarity_rename_pairs(datasets_full, renames), symlink: true)
+copy_files(motif_similarity_rename_pairs(dataset_ids_renames), symlink: true)
+
+hocomoco_similarities_renamed = File.readlines('hocomoco_similarities.tsv').map{|l| l.chomp.split("\t") }.map{|row|
+  [renamed_motif_basename(row[0], dataset_ids_renames), *row[1..-1]]
+}
+
+File.open('hocomoco_similarities_recalc.tsv', 'w'){|fw|
+  hocomoco_similarities_renamed.each{|row|
+    fw.puts(row.join("\t")
+  }
+}
